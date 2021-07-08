1084ddeebe315c16a5eab7ffd8f1f666d29a1b55
桃 <tao.zheng8833@hipac.cn>
2021-07-08 14:03:52 +0800

    规则中心结果解析

--- 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
+++ 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
@@ -96,7 +96,7 @@ shop_base as (
     WHERE dayid='$v_date'
 ),
 
---门店服务人员信息
+--门店服务人员信息合并表
 shop_pool_server as (
     SELECT shop_id,
            concat_ws(',' , sort_array(collect_set(cast(group_id as string)))) as group_id,
@@ -106,6 +106,17 @@ shop_pool_server as (
     AND is_deleted = 0
     AND is_enabled = 0
     group by shop_id
+),
+
+--门店服务人员信息临时表
+shop_pool_server_temp as (
+    SELECT shop_id as temp_shop_id,
+           group_id as temp_group_id,
+           user_id as temp_user_id
+    FROM dwd_shop_pool_server_d
+    WHERE dayid='$v_date'
+    AND is_deleted = 0
+    AND is_enabled = 0
 ),
 
 --门店分组关系
@@ -174,8 +185,8 @@ rule_execute_result as (
                      'bu_id', order_base.bu_id,
                      'is_pickup_pay_order', order_base.is_pickup_pay_order,
                      'supply_id', order_base.supply_id,
-                     'category_ids', CONCAT(order_base.category_1st_id, order_base.category_2nd_id, order_base.category_3rd_id),
-                     'pickup_category_ids', CONCAT(order_base.performance_category_1st_id, order_base.performance_category_2nd_id, order_base.performance_category_3rd_id),
+                     'category_ids',  CONCAT(ifnull(order_base.category_1st_id, 0), ',', ifnull(order_base.category_2nd_id, 0), ',', ifnull(order_base.category_3rd_id, 0)),
+                     'pickup_category_ids', CONCAT(ifnull(order_base.performance_category_1st_id,0), ',', ifnull(order_base.performance_category_2nd_id, 0), ',', ifnull(order_base.performance_category_3rd_id, 0)),
                      'item_ab_type', order_base.item_style,
                      'shop_id', order_base.shop_id,
                      'user_ids', shop_pool_server.user_id,
@@ -232,8 +243,16 @@ SELECT order_id,
 
        get_json_object(rule_execute_result, "$.knowledgePackageId") as knowledge_package_id,
        get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.ruleId") as result_rule_id,
-       get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.userId") as result_user_id
+       case when get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.no_channel") = 'true'
+                then '无归属'
+            when get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.user_id") != null
+                then get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.userId")
+            when get_json_object(get_json_object(rule_execute_result, "$.resultData"), "$.user_feature") != null
+                then shop_pool_server_temp.user_id
+            end as result_user_id
 FROM rule_execute_result
+LEFT JOIN shop_pool_server_temp ON rule_execute_result.shop_id = shop_pool_server_temp.temp_shop_id
+    and get_json_object(get_json_object(rule_execute_result.rule_execute_result, "$.resultData"), "$.user_feature") = cast(shop_pool_server_temp.temp_group_id as string)
 " &&
 
 exit 0
