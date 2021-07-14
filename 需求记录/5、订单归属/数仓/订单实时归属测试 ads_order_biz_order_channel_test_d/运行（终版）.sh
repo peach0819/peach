c5dd4b45a0ebfb29739af578d769e6ae097ebde2
桃 <tao.zheng8833@hipac.cn>
2021-07-14 17:42:33 +0800

    feat: 修改取服务商经理的逻辑

--- 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
+++ 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
@@ -170,6 +170,9 @@ SELECT order_id,
             when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_id') != null
                 then get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.userId')
             when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != null
+                 and get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') = '0'
+                then sp_operator_id
+            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != null
                 then shop_pool_server_temp.temp_user_id
             end as result_user_id
 FROM rule_execute_result
