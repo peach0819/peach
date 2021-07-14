cbf199329f988a5eff05b702226310beb8fcf3f1
桃 <tao.zheng8833@hipac.cn>
2021-07-14 20:40:41 +0800

    feat: 修改查询顺序

--- 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
+++ 需求记录/5、订单归属/数仓/订单实时归属测试 ads_order_biz_order_channel_test_d/运行（终版）.sh
@@ -131,6 +131,7 @@ SELECT order_id,
        trade_no,
        order_place_time,
        shop_id,
+       shop_name,
        sale_dc_id,
        sale_dc_id_name,
        bu_id,
@@ -139,10 +140,10 @@ SELECT order_id,
        supply_id,
        supply_name,
        category_1st_id,
-       category_2nd_id,
-       category_3rd_id,
        category_1st_name,
+       category_2nd_id,
        category_2nd_name,
+       category_3rd_id,
        category_3rd_name,
        performance_category_1st_id,
        performance_category_1st_name,
@@ -152,7 +153,6 @@ SELECT order_id,
        performance_category_3rd_name,
        item_style,
        item_style_name,
-       shop_name,
        store_type,
        sub_store_type,
        sp_id,
@@ -161,7 +161,6 @@ SELECT order_id,
        shop_pool_server_group_id,
        shop_pool_server_user_id,
        shop_group_id,
-       rule_execute_result,
 
        get_json_object(rule_execute_result, '$.knowledgePackageId') as knowledge_package_id,
        get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.ruleId') as result_rule_id,
@@ -174,7 +173,8 @@ SELECT order_id,
                 then sp_operator_id
             when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != null
                 then shop_pool_server_temp.temp_user_id
-            end as result_user_id
+            end as result_user_id,
+       rule_execute_result
 FROM rule_execute_result
 LEFT JOIN shop_pool_server_temp ON rule_execute_result.shop_id = shop_pool_server_temp.temp_shop_id
     and get_json_object(get_json_object(rule_execute_result.rule_execute_result, '$.resultData'), '$.user_feature') = cast(shop_pool_server_temp.temp_group_id as string)
