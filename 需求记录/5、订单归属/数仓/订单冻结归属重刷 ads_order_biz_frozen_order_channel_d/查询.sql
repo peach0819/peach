ff6e551c724db5b9ef7215c98591e27f1297efc9
桃 <tao.zheng8833@hipac.cn>
2021-07-15 13:52:31 +0800

    feat: 把终版同步到梳理

--- 需求记录/5、订单归属/数仓/订单冻结归属重刷 ads_order_biz_frozen_order_channel_d/查询.sql
+++ 需求记录/5、订单归属/数仓/订单冻结归属重刷 ads_order_biz_frozen_order_channel_d/查询.sql
@@ -1,45 +1,9 @@
 --订单基础信息
 WITH order_base as (
-    SELECT order_id,
-           trade_id,
-           trade_no,
-           order_place_time,
-           shop_id,
-           sale_dc_id,
-           sale_dc_id_name,
-           bu_id,
-           bu_id_name,
-           is_pickup_pay_order,
-           supply_id,
-           supply_name,
-           category_1st_id,
-           category_2nd_id,
-           category_3rd_id,
-           category_1st_name,
-           category_2nd_name,
-           category_3rd_name,
-           performance_category_1st_id,
-           performance_category_1st_name,
-           performance_category_2nd_id,
-           performance_category_2nd_name,
-           performance_category_3rd_id,
-           performance_category_3rd_name,
-           item_style,
-           item_style_name
-    FROM dw_trd_order_d
+    SELECT *
+    FROM ads_order_biz_order_channel_detail_d
     WHERE dayid='$v_date'
-    AND order_place_time between from_unixtime(UNIX_TIMESTAMP(CONCAT('$v_date', ' 00:00:00'), 'yyyyMMdd HH:mm:ss'))
-                             AND from_unixtime(UNIX_TIMESTAMP(CONCAT('$v_date', ' 23:59:59'), 'yyyyMMdd HH:mm:ss'))
-),
-
---门店基础信息
-shop_base as (
-    SELECT shop_id,
-           shop_name,
-           store_type,
-           sub_store_type
-    FROM dwd_shop_d
-    WHERE dayid='$v_date'
+    AND substr(order_place_time,0,8)='$v_date'
 ),
 
 --门店服务人员信息基础表
@@ -70,27 +34,6 @@ shop_pool_server_temp as (
     FROM shop_pool_server_base
 ),
 
---门店分组关系
-shop_group_mapping as (
-    SELECT shop_id,
-           concat_ws(',' , sort_array(collect_set(cast(group_id as string)))) as group_id
-    FROM dwd_shop_group_mapping_d
-    WHERE dayid='$v_date'
-    AND is_deleted = 0
-    group by shop_id
-),
-
---服务商订单快照信息
-sp_order_snapshot as (
-    SELECT order_id,
-           sp_id,
-           sp_name,
-           operator_id
-    FROM dwd_sp_order_snapshot_d
-    WHERE dayid='$v_date'
-    AND is_deleted = 0
-),
-
 --规则执行
 rule_execute_result as (
     SELECT order_base.order_id,
@@ -103,6 +46,7 @@ rule_execute_result as (
            order_base.bu_id,
            order_base.bu_id_name,
            order_base.is_pickup_pay_order,
+           order_base.is_pickup_recharge_order,
            order_base.supply_id,
            order_base.supply_name,
            order_base.category_1st_id,
@@ -119,22 +63,22 @@ rule_execute_result as (
            order_base.performance_category_3rd_name,
            order_base.item_style,
            order_base.item_style_name,
-           shop_base.shop_name,
-           shop_base.store_type,
-           shop_base.sub_store_type,
-           sp_order_snapshot.sp_id,
-           sp_order_snapshot.sp_name,
-           sp_order_snapshot.operator_id as sp_operator_id,
+           order_base.shop_name,
+           order_base.store_type,
+           order_base.sub_store_type,
+           order_base.sp_id,
+           order_base.sp_name,
+           order_base.sp_operator_id,
            shop_pool_server.group_id     as shop_pool_server_group_id,
            shop_pool_server.user_id      as shop_pool_server_user_id,
-           shop_group_mapping.group_id   as shop_group_id,
+           order_base.shop_group_id,
            ytdw.rule_execute(
-               '32/prod',
+               '3/prod',
                 map(
                      'time', '$rule_month',
                      'sale_dc_id', order_base.sale_dc_id,
                      'bu_id', order_base.bu_id,
-                     'is_pickup_pay_order', order_base.is_pickup_pay_order,
+                     'is_pickup_pay_order', order_base.is_pickup_recharge_order,
                      'supply_id', order_base.supply_id,
                      'category_ids',  CONCAT(COALESCE(order_base.category_1st_id, 0), ',', COALESCE(order_base.category_2nd_id, 0), ',', COALESCE(order_base.category_3rd_id, 0)),
                      'pickup_category_ids', CONCAT(COALESCE(order_base.performance_category_1st_id,0), ',', COALESCE(order_base.performance_category_2nd_id, 0), ',', COALESCE(order_base.performance_category_3rd_id, 0)),
@@ -142,36 +86,35 @@ rule_execute_result as (
                      'shop_id', order_base.shop_id,
                      'user_ids', shop_pool_server.user_id,
                      'user_features', shop_pool_server.group_id,
-                     'store_type', case when shop_base.sub_store_type is null then shop_base.store_type else CONCAT(shop_base.store_type,',',shop_base.sub_store_type) end,
-                     'sp_id', sp_order_snapshot.sp_id,
-                     'group_ids', shop_group_mapping.group_id
+                     'store_type', case when order_base.sub_store_type is null then order_base.store_type else CONCAT(order_base.store_type,',',order_base.sub_store_type) end,
+                     'sp_id', order_base.sp_id,
+                     'group_ids', order_base.shop_group_id
                 )
            ) as rule_execute_result
     FROM order_base
-    LEFT JOIN shop_base ON order_base.shop_id = shop_base.shop_id
     LEFT JOIN shop_pool_server ON shop_pool_server.trade_id = order_base.trade_id
-    LEFT JOIN shop_group_mapping ON shop_group_mapping.shop_id = shop_base.shop_id
-    LEFT JOIN sp_order_snapshot ON order_base.order_id = sp_order_snapshot.order_id
 )
 
 INSERT OVERWRITE TABLE ads_order_biz_frozen_order_channel_d partition (dayid='$v_date')
 SELECT order_id,
+       shop_id,
        trade_id,
        trade_no,
        order_place_time,
-       shop_id,
+       shop_name,
        sale_dc_id,
        sale_dc_id_name,
        bu_id,
        bu_id_name,
        is_pickup_pay_order,
+       is_pickup_recharge_order,
        supply_id,
        supply_name,
        category_1st_id,
-       category_2nd_id,
-       category_3rd_id,
        category_1st_name,
+       category_2nd_id,
        category_2nd_name,
+       category_3rd_id,
        category_3rd_name,
        performance_category_1st_id,
        performance_category_1st_name,
@@ -181,7 +124,6 @@ SELECT order_id,
        performance_category_3rd_name,
        item_style,
        item_style_name,
-       shop_name,
        store_type,
        sub_store_type,
        sp_id,
@@ -190,17 +132,20 @@ SELECT order_id,
        shop_pool_server_group_id,
        shop_pool_server_user_id,
        shop_group_id,
-       rule_execute_result,
 
        get_json_object(rule_execute_result, '$.knowledgePackageId') as knowledge_package_id,
        get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.ruleId') as result_rule_id,
        case when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.no_channel') = 'true'
                 then '无归属'
-            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_id') != null
-                then get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.userId')
-            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != null
+            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_id') != ''
+                then get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_id')
+            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != ''
+                 and get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') = '0'
+                then sp_operator_id
+            when get_json_object(get_json_object(rule_execute_result, '$.resultData'), '$.user_feature') != ''
                 then shop_pool_server_temp.temp_user_id
-            end as result_user_id
+            end as result_user_id,
+       rule_execute_result
 FROM rule_execute_result
 LEFT JOIN shop_pool_server_temp ON rule_execute_result.trade_id = shop_pool_server_temp.temp_trade_id
     and get_json_object(get_json_object(rule_execute_result.rule_execute_result, '$.resultData'), '$.user_feature') = cast(shop_pool_server_temp.temp_group_id as string)
