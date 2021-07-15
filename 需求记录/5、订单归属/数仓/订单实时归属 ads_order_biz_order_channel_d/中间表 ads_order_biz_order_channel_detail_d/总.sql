ab877bf6b5c5a32a39dcdd7f2652928f46461182
桃 <tao.zheng8833@hipac.cn>
2021-07-15 14:45:12 +0800

    feat: 去除系统类目逻辑

--- 需求记录/5、订单归属/数仓/订单实时归属 ads_order_biz_order_channel_d/中间表 ads_order_biz_order_channel_detail_d/总.sql
+++ 需求记录/5、订单归属/数仓/订单实时归属 ads_order_biz_order_channel_d/中间表 ads_order_biz_order_channel_detail_d/总.sql
@@ -45,49 +45,36 @@ partitioned by (dayid string)
 row format delimited fields terminated by '\001'
 stored as orc;
 
---系统类目海拍客类目转换
-WITH category as (
-    SELECT id, name, biz_category_id
-    FROM dwd_category_d
-    WHERE dayid = '$v_date'
-),
-
 --订单基础信息
-order_base as (
-    SELECT order1.order_id,
-           order1.trade_id,
-           order1.trade_no,
-           order1.order_place_time,
-           order1.shop_id,
-           case when order1.sale_dc_id = -1 then 0 else order1.sale_dc_id end as sale_dc_id,
-           order1.sale_dc_id_name,
-           order1.bu_id,
-           order1.bu_id_name,
-           order1.is_pickup_pay_order,
-           order1.is_pickup_recharge_order,
-           order1.supply_id,
-           order1.supply_name,
-           c1.biz_category_id as category_1st_id,
-           c2.biz_category_id as category_2nd_id,
-           c3.biz_category_id as category_3rd_id,
-           order1.category_1st_name,
-           order1.category_2nd_name,
-           order1.category_3rd_name,
-           pc1.biz_category_id as performance_category_1st_id,
-           order1.performance_category_1st_name,
-           pc2.biz_category_id as performance_category_2nd_id,
-           order1.performance_category_2nd_name,
-           pc3.biz_category_id as performance_category_3rd_id,
-           order1.performance_category_3rd_name,
-           order1.item_style,
-           order1.item_style_name
-    FROM dw_trd_order_d order1
-    LEFT JOIN category c1 ON order1.category_1st_id = c1.id
-    LEFT JOIN category c2 ON order1.category_2nd_id = c2.id
-    LEFT JOIN category c3 ON order1.category_3rd_id = c3.id
-    LEFT JOIN category pc1 ON order1.performance_category_1st_id = pc1.id
-    LEFT JOIN category pc2 ON order1.performance_category_2nd_id = pc2.id
-    LEFT JOIN category pc3 ON order1.performance_category_3rd_id = pc3.id
+with order_base as (
+    SELECT order_id,
+           trade_id,
+           trade_no,
+           order_place_time,
+           shop_id,
+           case when sale_dc_id = -1 then 0 else sale_dc_id end as sale_dc_id,
+           sale_dc_id_name,
+           bu_id,
+           bu_id_name,
+           is_pickup_pay_order,
+           is_pickup_recharge_order,
+           supply_id,
+           supply_name,
+           category_1st_id,
+           category_2nd_id,
+           category_3rd_id,
+           category_1st_name,
+           category_2nd_name,
+           category_3rd_name,
+           performance_category_1st_id,
+           performance_category_1st_name,
+           performance_category_2nd_id,
+           performance_category_2nd_name,
+           performance_category_3rd_id,
+           performance_category_3rd_name,
+           item_style,
+           item_style_name
+    FROM dw_trd_order_d
     WHERE dayid='$v_date'
 ),
 
