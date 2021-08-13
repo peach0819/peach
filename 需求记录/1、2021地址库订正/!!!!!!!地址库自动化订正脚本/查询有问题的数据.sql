923a2ffce0caa7e9d6a9020ed92637bc22ef257e
桃 <>
2021-08-13 15:09:21 +0800

    res: hsp地址库自动化订正脚本

--- 需求记录/1、2021地址库订正/!!!!!!!地址库自动化订正脚本/查询有问题的数据.sql
+++ 需求记录/1、2021地址库订正/!!!!!!!地址库自动化订正脚本/查询有问题的数据.sql
@@ -1,19 +1,37 @@
+--t_sp_area_brand
 SELECT b.id
 FROM  yt_hsp.t_sp_area_brand b
 INNER JOIN yt_ustone.t_area a ON b.area_id = a.area_id
-WHERE a.is_invalid = 1
+WHERE a.status = 2
+AND a.inuse = 0
 AND b.status=1
 
-
+--t_sp_area
 SELECT b.id
 FROM  yt_hsp.t_sp_area b
 INNER JOIN yt_ustone.t_area a ON b.area_id = a.area_id
-WHERE a.is_invalid = 1
+WHERE a.status = 2
+AND a.inuse = 0
 AND b.status=1;
 
-
+--t_sp_user_area
 SELECT b.id
 FROM  yt_hsp.t_sp_user_area b
 INNER JOIN yt_ustone.t_area a ON b.area_id = a.area_id
-WHERE a.is_invalid = 1
+WHERE a.status = 2
+AND a.inuse = 0
 AND b.is_deleted=0;
+
+--t_sp_brand.area_cnt
+SELECT b.*
+FROM t_sp_brand b
+INNER JOIN (
+    SELECT count(*) as cnt, brand_id, sp_id
+    FROM t_sp_area_brand
+    WHERE is_deleted=0
+    and status=1
+    group by brand_id, sp_id
+) temp ON b.sp_id = temp.sp_id AND b.brand_id = temp.brand_id
+WHERE b.status = 1
+and b.is_deleted = 0
+AND b.area_cnt != temp.cnt
