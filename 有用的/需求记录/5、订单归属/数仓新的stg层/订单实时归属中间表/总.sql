cb67cedf686bfcedec13b1403b640db880bbce10
桃 <>
2021-08-18 19:18:24 +0800

    feat: 数仓环路

--- 有用的/需求记录/5、订单归属/数仓新的stg层/订单实时归属中间表/总.sql
+++ 有用的/需求记录/5、订单归属/数仓新的stg层/订单实时归属中间表/总.sql
@@ -120,24 +120,28 @@ item as (
 
 --hi卡
 hi_pickup as (
-    select serial.out_biz_id as trade_id,
-           template.hi_card_type,
-           row_number() over(partition by serial.out_biz_id order by serial.card_fund_serial_id desc) as rn --最近的模板id
-    from (
-        select out_biz_id,
-               template_card_id,
-               card_fund_serial_id
-        from ods_t_card_fund_serial_details_d
-        where dayid='$v_date'
-    ) serial
-    inner join (
-        select id,
-               hi_card_type
-        from ods_t_hi_card_template_d
-        where dayid ='$v_date'
-        and is_deleted <> 99
-    ) template on serial.template_card_id=template.id
-    having rn = 1
+    SELECT trade_id,
+           hi_card_type
+    FROM (
+        select serial.out_biz_id as trade_id,
+               template.hi_card_type,
+               row_number() over(partition by serial.out_biz_id order by serial.card_fund_serial_id desc) as rn --最近的模板id
+        from (
+            select out_biz_id,
+                   template_card_id,
+                   card_fund_serial_id
+            from ods_t_card_fund_serial_details_d
+            where dayid='$v_date'
+        ) serial
+        inner join (
+            select id,
+                   hi_card_type
+            from ods_t_hi_card_template_d
+            where dayid ='$v_date'
+              and is_deleted <> 99
+        ) template on serial.template_card_id=template.id
+    ) temp
+    WHERE rn = 1
 ),
 
 --原dw_trd_order_d解析
