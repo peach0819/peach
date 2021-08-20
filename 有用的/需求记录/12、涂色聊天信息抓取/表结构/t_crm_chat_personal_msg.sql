200a337bb64841ed63961703970846698b1be368
桃 <>
2021-08-20 13:46:06 +0800

    feat: 涂色聊天信息抓取-表结构定义

---
+++ 有用的/需求记录/12、涂色聊天信息抓取/表结构/t_crm_chat_personal_msg.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `t_crm_chat_personal_msg` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `creator` varchar(255) DEFAULT NULL COMMENT '提交人',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `editor` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `edit_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `is_deleted` tinyint(4) DEFAULT '0' COMMENT '已删除',
+  `from_type` tinyint(4) NOT NULL COMMENT '数据来源类型',
+  `from_id` bigint(10) NOT NULL COMMENT '数据来源表id',
+  `from_chat_id` bigint(10) COMMENT '消息发送者crm系统chat_id',
+  `to_chat_id` bigint(10) COMMENT '消息接收者crm系统chat_id',
+  `msg_content` text NOT NULL COMMENT '消息内容',
+  `msg_time` datetime NOT NULL COMMENT '消息时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_from_id` (`from_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='微信私聊消息';
--- 有用的/需求记录/12、涂色聊天信息抓取/表结构/t_crm_tuse_qw_group_msg.sql
+++ 有用的/需求记录/12、涂色聊天信息抓取/表结构/t_crm_tuse_qw_group_msg.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `t_crm_tuse_qw_personal_msg` (
+CREATE TABLE `t_crm_tuse_qw_group_msg` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `creator` varchar(255) DEFAULT NULL COMMENT '提交人',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -7,7 +7,7 @@ CREATE TABLE `t_crm_tuse_qw_personal_msg` (
   `is_deleted` tinyint(4) DEFAULT '0' COMMENT '已删除',
   `callback_id` bigint(20) NOT NULL COMMENT '回调记录id',
   `tuse_send_wx_id` varchar(100) NOT NULL COMMENT '发送者涂色微信id',
-  `tuse_receiver_wx_id` varchar(100) NOT NULL COMMENT '接收者涂色微信id',
+  `tuse_group_id` varchar(100) NOT NULL COMMENT '涂色微信群id',
   `tuse_file_id` varchar(100) COMMENT '涂色文件id',
   `tuse_msg_type` int(10) COMMENT '涂色微信消息类型',
   `tuse_msg_id` varchar(100) NOT NULL COMMENT '涂色微信消息id',
@@ -16,5 +16,5 @@ CREATE TABLE `t_crm_tuse_qw_personal_msg` (
   `tuse_msg_time` datetime NOT NULL COMMENT '涂色消息时间',
   PRIMARY KEY (`id`),
   KEY `idx_callback_id` (`callback_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='涂色私聊消息';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='涂色群聊消息';
 
