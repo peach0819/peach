0b2fb3c60061d73678675697422c7f9b74debfb3
桃 <>
2021-08-20 10:03:32 +0800

    feat: 涂色聊天信息抓取

--- 有用的/需求记录/12、涂色聊天信息抓取/表结构/t_crm_tuse_callback_msg.sql
+++ 有用的/需求记录/12、涂色聊天信息抓取/表结构/t_crm_tuse_callback_msg.sql
@@ -5,9 +5,10 @@ CREATE TABLE `t_crm_tuse_callback_msg` (
   `editor` varchar(255) DEFAULT NULL COMMENT '修改人',
   `edit_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `is_deleted` tinyint(4) DEFAULT '0' COMMENT '已删除',
-  `type` bigint(10) NOT NULL COMMENT '回调消息类型',
+  `type` bigint(10) NULL COMMENT '回调消息类型',
   `type_desc` varchar(100) COMMENT '回调消息类型描述',
   `is_processed` tinyint(4) DEFAULT '0' COMMENT '已处理',
+  `biz_id` varchar(100) COMMENT '回调消息业务id',
   `content` text COMMENT '消息内容',
   PRIMARY KEY (`id`),
   KEY `idx_type` (`type`)
