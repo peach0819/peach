32c9724f61f66a1e888e9dc358a3db11d042c563
桃 <>
2021-09-01 19:11:52 +0800

    feat: 门店消息统计修改

--- 有用的/需求记录/12、涂色聊天信息抓取/数据分析/门店的消息.sql
+++ 有用的/需求记录/12、涂色聊天信息抓取/数据分析/门店的消息.sql
@@ -2,7 +2,7 @@
 
 SELECT msg.id, shop.shop_id
 FROM yt_crm.t_crm_tuse_qw_personal_msg msg
-INNER JOIN yt_crm.t_crm_chat customer ON msg.from_chat_id = customer.id and customer.type = 1
+INNER JOIN yt_crm.t_crm_chat customer ON msg.from_chat_id = customer.id and customer.qw_external_user_id is not null
 INNER JOIN yt_crm.t_crm_chat_bind bind ON customer.id = bind.chat_id and bind.type = 2
 INNER JOIN yt_ustone.t_linker linker ON linker.linker_id = bind.biz_id
 INNER JOIN yt_ustone.t_shop shop ON linker.linker_obj_id = shop.shop_id
