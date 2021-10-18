aec45229f40280955e6d62403cbc1d806f3851d7
桃 <>
2021-10-18 15:43:31 +0800

    feat: 来源表取值

--- 有用的/需求记录/5、订单归属/数仓新的stg层/订单实时归属中间表/总.sql
+++ 有用的/需求记录/5、订单归属/数仓新的stg层/订单实时归属中间表/总.sql
@@ -118,12 +118,12 @@ pickup_trade as (
                    row_number() over (partition by out_biz_id order by out_biz_id) as rn
             from (
                 select trade_id
-                from dwd_trade_shop_d
+                from ods_pt_trade_shop_d
                 where dayid='$v_date' and is_deleted=0 and trade_type=1  --卡票券充值
             ) trade
             inner join (
                 select out_biz_id,template_card_id
-                from dwd_card_fund_serial_details_d
+                from ods_t_card_fund_serial_details_d
                 where dayid='$v_date'
             ) serial on serial.out_biz_id=trade.trade_id
             inner join (
@@ -131,7 +131,7 @@ pickup_trade as (
                        get_json_object(hi_card_temp_ext,'$.firstPickupCategoryId') as firstpickupcategoryid,
                        get_json_object(hi_card_temp_ext,'$.secondaryPickupCategoryId') as secondarypickupcategoryid,
                        get_json_object(hi_card_temp_ext,'$.tertiaryPickupCategoryId') as tertiarypickupcategoryid
-                from dwd_hi_card_template_d
+                from ods_t_hi_card_template_d
                 where dayid ='$v_date'
                 and hi_card_type=1 --0 hi卡 1 提货卡
             ) template on serial.template_card_id=template.id
