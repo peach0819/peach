a9876702fb5bec80eeebb6336386d948d7b1fe09
桃 <>
2021-11-16 14:30:38 +0800

    逆向校验sql

--- 有用的/需求记录/14、特殊提成新架构/校验工具/校验逆向plan_sum_d/校验sql.sql
+++ 有用的/需求记录/14、特殊提成新架构/校验工具/校验逆向plan_sum_d/校验sql.sql
@@ -13,12 +13,35 @@ when coalesce(a.sts_target,0) <> coalesce(b.sts_target,0) then concat('少量差
 case when coalesce(a.commission_cap,'#') <> coalesce(b.commission_cap,'#') then concat('存在差异【',a.commission_cap,'：',b.commission_cap,'】') else '无差异' end as commission_cap_diff_flg,
 case when coalesce(a.commission_plan_type,'#') <> coalesce(b.commission_plan_type,'#') then concat('存在差异【',a.commission_plan_type,'：',b.commission_plan_type,'】') else '无差异' end as commission_plan_type_diff_flg,
 case when coalesce(a.commission_reward_type,'#') <> coalesce(b.commission_reward_type,'#') then concat('存在差异【',a.commission_reward_type,'：',b.commission_reward_type,'】') else '无差异' end as commission_reward_type_diff_flg,
-case when coalesce(a.cur_commission_reward,'#') <> coalesce(b.cur_commission_reward,'#') then concat('存在差异【',a.cur_commission_reward,'：',b.cur_commission_reward,'】') else '无差异' end as cur_commission_reward_diff_flg,
-case when coalesce(a.pre_commission_reward,'#') <> coalesce(b.pre_commission_reward,'#') then concat('存在差异【',a.pre_commission_reward,'：',b.pre_commission_reward,'】') else '无差异' end as pre_commission_reward_diff_flg,
-case when coalesce(a.commission_reward_change,'#') <> coalesce(b.commission_reward_change,'#') then concat('存在差异【',a.commission_reward_change,'：',b.commission_reward_change,'】') else '无差异' end as commission_reward_change_diff_flg
- from ( select * from dw_salary_backward_plan_sum_d where dayid='$v_date' ) a
 
-left join (select * from dw_salary_backward_plan_sum_new_d where dayid='$v_date' ) b on 1=1
+case when commission_reward_type = '实物' then (
+        case when coalesce(a.cur_commission_reward,'#') <> coalesce(b.cur_commission_reward,'#')
+        then concat('存在差异【',a.cur_commission_reward,'：',b.cur_commission_reward,'】')
+        else '无差异' end
+     )
+     when commission_reward_type = '金额' then (
+        case when abs(coalesce(a.cur_commission_reward,0) - coalesce(b.cur_commission_reward,0))>0.0003
+        then concat('存在差异【',a.cur_commission_reward,'：',coalesce(b.cur_commission_reward,'】')
+        else '无差异' end
+     )
+     end as cur_commission_reward_diff_flg,
+
+case when commission_reward_type = '实物' then (
+        coalesce(a.pre_commission_reward,'#') <> coalesce(b.pre_commission_reward,'#')
+        then concat('存在差异【',a.pre_commission_reward,'：',b.pre_commission_reward,'】')
+        else '无差异'
+     )
+     when commission_reward_type = '金额' then (
+        case when abs(coalesce(a.pre_commission_reward,0) - coalesce(b.pre_commission_reward,0))>0.0003
+        then concat('存在差异【',a.pre_commission_reward,'：',coalesce(b.pre_commission_reward,'】')
+        else '无差异' end
+     )
+     end as cur_commission_reward_diff_flg,
+
+case when coalesce(a.commission_reward_change,'#') <> coalesce(b.commission_reward_change,'#') then concat('存在差异【',a.commission_reward_change,'：',b.commission_reward_change,'】') else '无差异' end as commission_reward_change_diff_flg
+ from ( select * from dw_salary_backward_plan_sum_d where dayid='20211031' ) a
+
+left join (select * from dw_salary_backward_plan_sum_new_d where dayid='20211031' ) b on 1=1
  and coalesce(a.plan_no,'#') = coalesce(b.plan_no,'#')
  and coalesce(a.grant_object_user_id,'#') = coalesce(b.grant_object_user_id,'#')
  and coalesce(a.planno,'#') = coalesce(b.planno,'#')
