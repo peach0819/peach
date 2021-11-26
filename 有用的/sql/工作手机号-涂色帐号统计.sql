a4a88da9b9ad6e0bb6fb0fc448705a4391a0f288
桃 <>
2021-11-26 18:23:52 +0800

    feat: 工作手机号-涂色帐号统计.sql

---
+++ 有用的/sql/工作手机号-涂色帐号统计.sql
@@ -0,0 +1,8 @@
+SELECT  phone.phone as 工作手机号,
+        phone.bind_tu_account as 涂色帐号,
+        phone.bind_tu_password as 涂色密码,
+        admin.full_name as 绑定电销名,
+        dept.name as 电销组名
+FROM t_crm_work_phone phone
+LEFT JOIN yt_ustone.t_user_admin admin ON phone.bind_user_id = admin.user_id
+LEFT JOIN yt_ustone.t_department dept ON dept.id = admin.dept_id
