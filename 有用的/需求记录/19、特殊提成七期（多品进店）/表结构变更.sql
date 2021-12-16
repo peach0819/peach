3d0359ae68c967432bbd153d8cecf1e67f17bfc2
桃 <>
2021-12-16 15:14:55 +0800

    sql

---
+++ 有用的/需求记录/19、特殊提成七期（多品进店）/表结构变更.sql
@@ -0,0 +1,4 @@
+
+update bounty_filter set component_value_config = '{\"type\":\"custom-area\",\"url\": \"hipac.crm.bountyPlan.listSalesArea\",\"code\":\"WAR_AREA\"}' WHERE id = 12;
+update bounty_filter set component_value_config = '{\"type\":\"custom-area\",\"url\": \"hipac.crm.bountyPlan.listSalesArea\",\"code\":\"BD_AREA\"}' WHERE id = 13;
+update bounty_filter set component_value_config = '{\"type\":\"custom-area\",\"url\": \"hipac.crm.bountyPlan.listSalesArea\",\"code\":\"MANAGE_AREA\"}' WHERE id = 14;
