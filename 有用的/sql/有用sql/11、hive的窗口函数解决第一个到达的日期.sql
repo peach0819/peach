622402a2fe9e07ae07122434de8779c379e8a3b9
桃 <>
2021-12-17 14:25:13 +0800

    sql

--- 有用的/sql/有用sql/11、hive的窗口函数解决第一个到达的日期.sql
+++ 有用的/sql/有用sql/11、hive的窗口函数解决第一个到达的日期.sql
@@ -41,5 +41,15 @@ with t as (
     SELECT 2 as id, 110 as user_id, 10 as value
 )
 
-SELECT id, value , sum(value) over (partition by id order by user_id  )
+SELECT id, value , sum(value) over (partition by id order by user_id  ),
 FROM t
+
+lag(exp_str,offset,defval) over(partion by ..order by …)
+
+lead(exp_str,offset,defval) over(partion by ..order by …)
+
+其中exp_str是字段名
+
+     Offset是偏移量，即是上1个或上N个的值，假设当前行在表中排在第5行，则offset 为3，则表示我们所要找的数据行就是表中的第2行（即5-3=2）。
+
+     Defval默认值，当两个函数取上N/下N个值，当在表中从当前行位置向前数N行已经超出了表的范围时，lag（）函数将defval这个参数值作为函数的返回值，若没有指定默认值，则返回NULL，那么在数学运算中，总要给一个默认值才不会出错。
