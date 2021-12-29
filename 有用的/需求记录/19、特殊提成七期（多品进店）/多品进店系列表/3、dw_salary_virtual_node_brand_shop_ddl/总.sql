2dd060039b5d707ed3a7dcf8505b53e65efdade8
桃 <>
2021-12-29 10:43:50 +0800

    feat: 比对周期明细表

--- 有用的/需求记录/19、特殊提成七期（多品进店）/多品进店系列表/3、dw_salary_virtual_node_brand_shop_ddl/总.sql
+++ 有用的/需求记录/19、特殊提成七期（多品进店）/多品进店系列表/3、dw_salary_virtual_node_brand_shop_ddl/总.sql
@@ -1,5 +1,5 @@
 use ytdw;
-create table dw_salary_brand_shop_current_public_d
+create table if not exists dw_salary_brand_shop_current_public_d
 (
     planno                        int comment '方案编号',
     plan_month                    string comment '方案月份',
@@ -28,4 +28,33 @@ create table dw_salary_brand_shop_current_public_d
     current_total_gmv_less_refund decimal(18, 2) comment '截止当前订单的gmv-退款总和'
 ) comment '多品进店当前周期明细表'
 partitioned by (dayid string, pltype string)
+stored as orc;
+
+create table if not exists dw_salary_brand_shop_compare_public_d
+(
+    planno            int comment '方案编号',
+    plan_month        string comment '方案月份',
+    update_time       string comment '更新时间',
+    update_month      string comment '执行月份',
+    order_id          string comment '订单id',
+    trade_id          string comment '交易id',
+    pay_date          string comment '支付日期',
+    rfd_date          string comment '退款日期',
+    gmv               decimal(18, 2) comment '实货gmv',
+    refund            decimal(18, 2) comment '退款',
+    gmv_less_refund   decimal(18, 2) comment '实货gmv-退款',
+    shop_id           string comment '门店ID',
+    shop_name         string comment '门店名称',
+    brand_id          bigint comment '商品品牌id',
+    brand_name        string comment '商品品牌',
+    category_1st_id   bigint comment '商品一级类目id',
+    category_1st_name string comment '商品一级类目',
+    category_2nd_id   bigint comment '商品二级类目id',
+    category_2nd_name string comment '商品二级类目',
+    item_id           bigint comment '商品ID',
+    item_name         string comment '商品名称',
+    item_style        tinyint comment 'AB类型',
+    item_style_name   string comment 'AB类型名称'
+) comment '多品进店比对周期明细表'
+partitioned by (dayid string)
 stored as orc;
---
+++ 有用的/需求记录/19、特殊提成七期（多品进店）/多品进店系列表/4、比对周期表/1、dw_salary_brand_shop_compare_public_d（比对周期明细）/运行.sh
@@ -0,0 +1,108 @@
+v_date=$1
+
+source ../sql_variable.sh $v_date
+source ../yarn_variable.sh dw_salary_forward_brand_shop_current_public_d '肥桃'
+
+spark-sql $spark_yarn_job_name_conf $spark_yarn_queue_name_conf --master yarn --executor-memory 4G --num-executors 4 -v -e "
+use ytdw;
+
+with plan as (
+    SELECT no,
+           month,
+           get_json_object(get_json_object(filter_config_json,'$.item_style'),'$.value') as item_style_value,
+           get_json_object(get_json_object(filter_config_json,'$.item_style'),'$.operator') as item_style_operator,
+           get_json_object(get_json_object(filter_config_json,'$.category_first'),'$.value') as category_first_value,
+           get_json_object(get_json_object(filter_config_json,'$.category_first'),'$.operator') as category_first_operator,
+           get_json_object(get_json_object(filter_config_json,'$.category_second'),'$.value') as category_second_value,
+           get_json_object(get_json_object(filter_config_json,'$.category_second'),'$.operator') as category_second_operator,
+           get_json_object(get_json_object(filter_config_json,'$.brand'),'$.value') as brand_value,
+           get_json_object(get_json_object(filter_config_json,'$.brand'),'$.operator') as brand_operator,
+           get_json_object(get_json_object(filter_config_json,'$.war_area'),'$.value') as war_area_value,
+           get_json_object(get_json_object(filter_config_json,'$.war_area'),'$.operator') as war_area_operator,
+           get_json_object(get_json_object(filter_config_json,'$.bd_area'),'$.value') as bd_area_value,
+           get_json_object(get_json_object(filter_config_json,'$.bd_area'),'$.operator') as bd_area_operator,
+           get_json_object(get_json_object(filter_config_json,'$.manage_area'),'$.value') as manage_area_value,
+           get_json_object(get_json_object(filter_config_json,'$.manage_area'),'$.operator') as manage_area_operator,
+           get_json_object(get_json_object(filter_config_json,'$.sales_team'),'$.value') as sales_team_value,
+           get_json_object(get_json_object(filter_config_json,'$.sales_team'),'$.operator') as sales_team_operator,
+           get_json_object(get_json_object(filter_config_json,'$.shop_group'),'$.value') as shop_group_value,
+           get_json_object(get_json_object(filter_config_json,'$.shop_group'),'$.operator') as shop_group_operator,
+           replace(replace(replace(split(get_json_object(get_json_object(filter_config_json,'$.compare_date'),'$.value'),',')[0],'[',''),'\"',''),'-','') as calculate_date_value_start,
+           replace(replace(replace(split(get_json_object(get_json_object(filter_config_json,'$.compare_date'),'$.value'),',')[1],']',''),'\"',''),'-','') as calculate_date_value_end
+    FROM dw_bounty_plan_schedule_d
+    WHERE dayid = '$v_date'
+    AND array_contains(split(forward_date, ','), '$v_date')
+    AND bounty_rule_type = 4
+),
+
+cur as (
+    SELECT from_unixtime(unix_timestamp(),'yyyy-MM-dd HH:mm:ss') as update_time,
+           from_unixtime(unix_timestamp(),'yyyy-MM') as update_month,
+           plan.no as planno,
+           plan.month as plan_month,
+           ord.order_id,
+           ord.trade_id,
+           ord.pay_date,
+           ord.rfd_date,
+           ord.gmv,
+           ord.refund,
+           ord.gmv_less_refund,
+           ord.shop_id,
+           ord.shop_name,
+           ord.brand_id,
+           ord.brand_name,
+           ord.category_1st_id,
+           ord.category_1st_name,
+           ord.category_2nd_id,
+           ord.category_2nd_name,
+           ord.item_id,
+           ord.item_name,
+           ord.item_style,
+           ord.item_style_name,
+           case when plan.bounty_payout_object_code= 'WAR_ZONE_MANAGE' then war_zone_id
+                when plan.bounty_payout_object_code= 'AREA_MANAGER' then area_manager_id
+                when plan.bounty_payout_object_code= 'BD_MANAGER' then bd_manager_id
+                when plan.bounty_payout_object_code= 'BD' then if(plan.payout_object_type = '冻结', ord.frozen_sale_user_id, ord.newest_sale_user_id)
+           end as grant_object_user_id
+    FROM plan
+    CROSS JOIN (select * from dw_salary_brand_shop_rule_public_mid_v2_d where dayid ='$v_date') ord ON 1=1
+    WHERE ord.pay_date between plan.calculate_date_value_start and plan.calculate_date_value_end
+    AND ytdw.simple_expr(ord.item_style_name, 'in', plan.item_style_value) = if(plan.item_style_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.category_1st_id, 'in', plan.category_first_value) = if(plan.category_first_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.category_2nd_id, 'in', plan.category_second_value) = if(plan.category_second_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.brand_id, 'in', plan.brand_value) = if(plan.brand_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.war_zone_dep_id, 'in', plan.war_area_value) = if(plan.war_area_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.area_manager_dep_id, 'in', plan.bd_area_value) = if(plan.bd_area_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.bd_manager_dep_id, 'in', plan.manage_area_value) = if(plan.manage_area_operator = '=', 1, 0)
+    AND ytdw.simple_expr(ord.sale_team_id, 'in', plan.sales_team_value) = if(plan.sales_team_operator = '=', 1, 0)
+    AND if(ord.shop_group = '' OR plan.shop_group_value = '', 0, ytdw.simple_expr(substr(plan.shop_group_value, 2, length(plan.shop_group_value) - 2), 'in', concat('[', ord.shop_group, ']'))) = if(plan.shop_group_operator = '=', 1, 0)
+)
+
+insert overwrite table dw_salary_brand_shop_compare_public_d partition (dayid='$v_date')
+SELECT planno,
+       plan_month,
+       update_time,
+       update_month,
+       order_id,
+       trade_id,
+       pay_date,
+       rfd_date,
+       gmv,
+       refund,
+       gmv_less_refund,
+       shop_id,
+       shop_name,
+       brand_id,
+       brand_name,
+       category_1st_id,
+       category_1st_name,
+       category_2nd_id,
+       category_2nd_name,
+       item_id,
+       item_name,
+       item_style,
+       item_style_name
+FROM cur
+" &&
+
+exit 0
