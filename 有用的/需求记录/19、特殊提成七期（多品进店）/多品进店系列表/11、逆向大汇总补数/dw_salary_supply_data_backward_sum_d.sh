73ee4ef11cc0989dbcfe36da242d2224507900a7
桃 <>
2022-01-04 13:59:56 +0800

    feat: 逆向大汇总补数

---
+++ 有用的/需求记录/19、特殊提成七期（多品进店）/多品进店系列表/11、逆向大汇总补数/dw_salary_supply_data_backward_sum_d.sh
@@ -0,0 +1,26 @@
+v_date=$1
+
+source ../sql_variable.sh $v_date
+
+function supply_backward() {
+    supply_date=$1
+    data_date=$2
+    sh ./dw_salary_backward_plan_sum_d.sh $data_date $supply_date
+}
+
+backward_supply_date=$(apache-spark-sql -e "
+    use ytdw;
+    SELECT concat_ws('\;' , sort_array(collect_set(cast(need_supply_date as int))))
+    FROM dw_bounty_plan_schedule_d
+    lateral view explode(split(backward_date,',')) temp as need_supply_date
+    WHERE supply_date = '$v_date'
+    AND need_supply_date < '$v_date'
+")
+
+echo backward_supply_date: $backward_supply_date
+
+IFS=";"
+for data_date in ${backward_supply_date[@]}
+do
+    supply_backward $v_date $data_date
+done
