fe5ac2f0a271e774e7cf820f23fd7f169b3d3172
桃 <>
2022-03-14 13:38:23 +0800

    ddl

---
+++ 有用的/需求记录/23、企微官方发送接入/ddl.sql
@@ -0,0 +1,8 @@
+
+
+
+ALTER TABLE t_touch_task ADD COLUMN channel_type tinyint(4) NOT NULL DEFAULT '0' COMMENT '发送渠道 0涂色通道 1企微官方通道';
+ALTER TABLE t_touch_task_detail ADD COLUMN qw_msg_id varchar(200) COMMENT '企微官方通道消息id';
+ALTER TABLE t_touch_task_detail ADD COLUMN qw_msg_time datetime COMMENT '企微官方消息发送时间';
+
+ALTER TABLE t_touch_task_detail ADD KEY `idx_qw_msg_id` (`qw_msg_id`);
