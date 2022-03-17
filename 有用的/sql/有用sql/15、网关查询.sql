92bcc442df38a11a32590dee2a6608e34c70b242
桃 <>
2022-03-17 22:12:52 +0800

    统计.sql

---
+++ 有用的/sql/有用sql/15、网关查询.sql
@@ -0,0 +1,13 @@
+SELECT * FROM ytgw_apply_detail_communication;
+
+SELECT * FROM ytgw_apply_detail WHERE trans_type_id = 'WEIXIN06100102'
+
+SELECT * FROM  ytgw_out_trans_config;
+
+SELECT * FROM ytgw_assemble_template ORDER BY id DESC;
+
+SELECT * FROM ytgw_communication_map ORDER BY id DESC;
+
+SELECT * FROM ytgw_out_trans_code ORDER BY id desc
+
+
