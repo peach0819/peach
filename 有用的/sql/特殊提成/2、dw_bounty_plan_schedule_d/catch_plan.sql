f90b3d27920ed0309ed774e7279ceb022e976e06
桃 <>
2022-04-18 10:36:08 +0800

    move

--- 有用的/sql/特殊提成/2、dw_bounty_plan_schedule_d/catch_plan.sql
+++ 有用的/sql/特殊提成/2、dw_bounty_plan_schedule_d/catch_plan.sql
@@ -2,7 +2,7 @@ v_date=$1
 
 source ../sql_variable.sh $v_date
 
-sh /alidata/workspace/yt_bigdata/edp/data_offline_backup/ods_bounty_plan_d.sh $v_date 'prod_slave_cloudatlas' &&
+sh /alidata/workspace/yt_bigdata/edp/data_offline_backup/ods_bounty_plan_d.sh $v_date 'prod_slave_crm' &&
 sh /alidata/workspace/yt_bigdata/edp/data_offline_backup/dwd_bounty_plan_d.sh $v_date &&
 sh /alidata/workspace/yt_bigdata/edp/data_special_salary_new/dw_bounty_plan_d.sh $v_date &&
 sh /alidata/workspace/yt_bigdata/edp/data_special_salary_new/dw_bounty_plan_schedule_d.sh $v_date &&
