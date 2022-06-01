80ef4971aaf4a4f674731456dc91d7298bf2440e
桃 <>
2022-06-01 12:37:24 +0800

    fix

--- 有用的/需求记录/29、特殊提成九期/数仓ddl.sql
+++ 有用的/需求记录/29、特殊提成九期/数仓ddl.sql
@@ -9,3 +9,5 @@ ALTER TABLE ytdw.dw_salary_gmv_rule_public_mid_v2_d add columns (pickup_recharge
 ALTER TABLE ytdw.dw_salary_gmv_rule_public_mid_v2_d add columns (pickup_recharge_pay_amount decimal(18, 2) comment '提货卡充值支付金额') CASCADE;
 ALTER TABLE ytdw.dw_salary_gmv_rule_public_mid_v2_d add columns (pickup_recharge_pay_amount_less_refund decimal(18, 2) comment '提货卡充值支付金额-退款') CASCADE;
 ALTER TABLE ytdw.dw_salary_gmv_rule_public_d add columns (extra string comment '冗余字段') CASCADE;
+
+ALTER TABLE ytdw.dw_salary_gmv_rule_public_mid_v2_d add columns (is_pickup_recharge_order int comment '是否为充值提货hi卡订单 1 是 0 否') CASCADE;
