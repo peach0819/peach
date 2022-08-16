8ec76f5b55ea8d623908e4b86ae6c33b52af71e8
桃 <>
2022-08-16 10:57:02 +0800

    ref

--- 有用的/create table语句/6、t_crm_chat_group.sql
+++ 有用的/create table语句/6、t_crm_chat_group.sql
@@ -11,6 +11,11 @@ CREATE TABLE `t_crm_chat_group` (
   `group_name` varchar(100) DEFAULT '' COMMENT '群名称',
   `owner_qw_user_id` varchar(50) NOT NULL DEFAULT '' COMMENT '群主id',
   `owner_chat_id` bigint(32) DEFAULT NULL COMMENT '群主聊天实体id（t_crm_chat主键）',
+
+  `open_user_id` varchar(50) COMMENT '开通群的用户id',
+  `open_chat_id` bigint(32) COMMENT '开通群的用户chatId',
+  `belong_user_id` varchar(2000) COMMENT '所属用户id jsonarray',
+  `belong_chat_id` varchar(2000) COMMENT '所属用户chatId jsonarray',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_group_id` (`group_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=862 DEFAULT CHARSET=utf8mb4 COMMENT='群聊'
