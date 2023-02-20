a4ecfb29e2f70157d7b766cfc307c0baf2738899
桃 <>
2023-02-20 15:21:31 +0800

    1

--- 没用的/genericSql格式testsql.sql
+++ 没用的/genericSql格式testsql.sql
@@ -1,138 +1,159 @@
-
-
-use ytdw;
-
-create table if not exists ads_crm_visit_record_qw_msg_d_test
-(
-  record_id string comment '拜访小记ID',
-  admin_chat_id bigint comment '小二crm系统聊天主体chat_id',
-  shop_chat_id bigint comment '门店crm系统聊天主体chat_id',
-  wx_msg_id bigint comment '企微的chat_id',
-  direction int comment '发送类型 1： 小二发送给门店，2门店发送给小二',
-  msg_time string comment '消息时间',
-  msg_type string comment '微信消息类型',
-  content string comment '微信消息内容',
-  extend string comment '其他',
-  room_id string comment '群聊群id',
-  is_qw int comment '1:企微渠道'
+create table if not exists ytdw.dw_salary_shop_data_d (
+                                                          shop_id string comment '门店id',
+                                                          shop_name string comment '门店名称',
+                                                          shop_pro_name string comment '门店省',
+                                                          shop_city_name string comment '门店市',
+                                                          shop_area_name string comment '门店区',
+                                                          service_user_id string comment 'B有效新开销售id',
+                                                          service_user_name string comment 'B有效新开销售名',
+                                                          service_department_name string comment 'B有效新开销售组',
+                                                          min_pay_month string comment '门店B首次下单月份',
+                                                          min_pay_day string comment '门店B首次下单日期',
+                                                          new_sign_pay_day string comment '门店B有效新开日期',
+                                                          new_sign_pure_pay_amount string comment '门店有效新开当日支付金额-订单退款',
+                                                          new_sign_pay_amount string comment '门店有效新开当日支付金额',
+                                                          new_sign_refund_actual_amount string comment '门店新开当日订单退款金额',
+                                                          service_info_freezed string comment '冻结服务人员信息',
+                                                          store_type_name string comment '门店类型',
+                                                          approve_status_name string comment '门店质审核状态',
+                                                          update_time string comment '更新时间'
 )
-partitioned by (dayid string)
-stored as orc;
-
-WITH records AS (
-		SELECT record_id,
-		   get_json_object(extra_json, '$.adminChatId') AS admin_chat_id,
-			get_json_object(extra_json, '$.shopChatId') AS shop_chat_id,
-			date_format(get_json_object(extra_json, '$.chatStartTime'), 'yyyyMMddHHmmss') AS chat_start_time,
-			date_format(get_json_object(extra_json, '$.chatEndTime'), 'yyyyMMddHHmmss') AS chat_end_time
-		FROM ytdw.dwd_visit_record_d
-		WHERE dayid = '$v_date'
-		AND record_type = 4
-		AND visit_type = 4
-		AND get_json_object(extra_json, '$.adminChatId') is not null
-		AND get_json_object(extra_json, '$.shopChatId') is not null
-		AND get_json_object(extra_json, '$.chatStartTime') is not null
-		AND get_json_object(extra_json, '$.chatEndTime') is not NULL
-		AND date_format(get_json_object(extra_json, '$.chatStartTime'), 'yyyyMMdd') = '$v_date'
-    ),
-	qw_chats AS (
-		SELECT id AS wx_msg_id, admin_chat_id, shop_chat_id, direction, msg_time AS wx_msg_time,
-			msg_type, content, extend AS extend, room_id, 1 AS is_qw
-		FROM ytdw.dwd_crm_chat_msg_qw_d
-		WHERE dayid = '$v_date'
-		AND admin_chat_id IS NOT NULL
-		AND shop_chat_id IS NOT NULL
-		AND is_deleted = 0
-		and substr(msg_time, 0, 8) = '$v_date'
-	),
-	tuse_chats AS (
-		SELECT id AS wx_msg_id, from_chat_id, to_chat_id, tuse_msg_time AS wx_msg_time, tuse_msg_type AS msg_type,
-			tuse_msg_content AS content, NULL AS extend, NULL AS room_id, 0 AS is_qw, 1 as tuse_tag
-		FROM ytdw.dwd_crm_tuse_qw_personal_msg_d
-		WHERE dayid = '$v_date'
-		AND is_deleted = 0
-		AND from_chat_id IS NOT NULL
-		AND to_chat_id IS NOT NULL
-		and substr(tuse_msg_time, 0, 8) = '$v_date'
-	),
-	channels AS (
-		SELECT records.record_id, records.admin_chat_id, records.shop_chat_id, records.chat_start_time, records.chat_end_time,
-			CASE WHEN qw_channel IS NOT NULL AND qw_channel = 1
-			 	 THEN 1
-				 ELSE 2
-			END AS chat_source
-		FROM records
-		LEFT JOIN (
-				SELECT record_id, record.admin_chat_id, record.shop_chat_id, record.chat_start_time, record.chat_end_time,
-					CASE
-					 WHEN count(*) > 0 THEN 1
-					 ELSE 0
-					END AS qw_channel
-				FROM qw_chats chat
-				INNER JOIN records record ON record.admin_chat_id = chat.admin_chat_id AND record.shop_chat_id = chat.shop_chat_id
-				WHERE chat.direction = 2
-				AND chat.wx_msg_time >= record.chat_start_time
-				AND chat.wx_msg_time <= record.chat_end_time
-				GROUP BY record_id, record.admin_chat_id, record.shop_chat_id, record.chat_start_time, record.chat_end_time
-			) is_qw_channel ON records.record_id = is_qw_channel.record_id
-	)
+    comment '销售薪资门店数据'
+    partitioned by (dayid string)
+    stored as orc;
 
 
-INSERT OVERWRITE TABLE ads_crm_visit_record_qw_msg_d_test PARTITION (dayid='$v_date')
-  SELECT record_id, record.admin_chat_id, record.shop_chat_id, wx_msg_id, direction,
-	ytdw.yt_date_format(wx_msg_time, 'yyyy-MM-dd HH:mm:ss') AS msg_time, msg_type,
-	content, extend, room_id, is_qw
-FROM (
-	SELECT *
-	FROM channels
-	WHERE chat_source = 1
-) record
-INNER JOIN qw_chats ON record.admin_chat_id = qw_chats.admin_chat_id AND record.shop_chat_id = qw_chats.shop_chat_id
-WHERE qw_chats.wx_msg_time >= chat_start_time
-AND qw_chats.wx_msg_time <= chat_end_time
+
+insert overwrite table ytdw.dw_salary_shop_data_d partition(dayid='$v_date')
+select
+    table1.shop_id,
+    table1.shop_name,
+    table1.shop_pro_name,
+    table1.shop_city_name,
+    table1.shop_area_name,
+    table2.service_user_id,
+    table2.service_user_name,
+    table2.service_department_name,
+    substr(table3.min_pay_day,1,6) as min_pay_month,
+    table3.min_pay_day,
+    table2.pay_day,
+    table2.pure_pay_amount,
+    table2.pay_amount,
+    table2.refund_actual_amount,
+    table2.service_info_freezed,
+    table1.store_type_name,
+    table1.approve_status_name,
+    from_unixtime(unix_timestamp()) as update_time
+from
 
 
-UNION ALL
+---有效新开数据
 
-SELECT record_id, record.admin_chat_id, record.shop_chat_id, wx_msg_id,
-	CASE
-	 WHEN admin_chat_id = from_chat_id THEN 1
-	 ELSE 2
-	END AS direction, ytdw.yt_date_format(wx_msg_time, 'yyyy-MM-dd HH:mm:ss') AS msg_time, msg_type,
-	content, extend, room_id, is_qw
-FROM (
-	SELECT *, 1 as tuse_tag
-	FROM channels
-	WHERE chat_source = 2
-) record
-left  JOIN tuse_chats ON ((admin_chat_id = from_chat_id AND shop_chat_id = to_chat_id) )
--- OR (admin_chat_id = to_chat_id AND shop_chat_id = from_chat_id))
-WHERE  tuse_chats.wx_msg_time >= chat_start_time
-AND tuse_chats.wx_msg_time <= chat_end_time
-and tuse_chats.tuse_tag is not null
-and from_chat_id is not null
-and to_chat_id is not null
+(
+    select
+        shop_id,
+        service_user_id,
+        service_user_name,
+        service_department_name,
+        service_info_freezed,
+        pay_day,
+        pay_amount,
+        refund_actual_amount,
+        pure_pay_amount
+    from
+        (
+            select
+                shop_id,
+                service_user_id,
+                service_user_name,
+                service_department_name,
+                service_info_freezed,
+                pay_day,
+                pay_amount,
+                refund_actual_amount,
+                pure_pay_amount,
+                row_number() over(partition by shop_id order by pay_day,pure_pay_amount desc) rn
+            from
+                (
+                    select
+                        shop_id,
+                        shop_pro_id,
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_user_id'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_user_id'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_user_id')) as service_user_id,
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_user_name'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_user_name'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_user_name')) as service_user_name,
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_department_name'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_department_name'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_department_name')) as service_department_name,
+                        service_info_freezed,
+                        substr(pay_time,1,8) as pay_day,
+                        sum(pay_amount) as pay_amount,
+                        sum(case when afs_refund.refund_status=9 then afs_refund.refund_actual_amount else 0 end) as refund_actual_amount,
+                        sum(pay_amount)-nvl(sum(case when afs_refund.refund_status=9 then afs_refund.refund_actual_amount else 0 end),0) as pure_pay_amount
 
-union all
+                    from (
+                             select order_id,shop_id,shop_pro_id,service_info_freezed,pay_time,pay_amount
+                             from ytdw.dw_order_d
+                             where dayid='$v_date'
+                               and pay_time is not null
+                               and bu_id=0
+                               and sp_id is null
+                               --排除员工店、伙伴店11 20200505
+                               and nvl(store_type,100) not in (3,9,10,11)
+                               and business_unit not in ('卡券票','其他')
+                               --B类
+                               and item_style_freezed=1
+                               and sale_dc_id=-1) order
+                        left join (
+	 --20200728
+	   select order_id
+             ,refund_status
+             ,sum(refund_actual_amount) as refund_actual_amount --实际退款金额
 
-
-SELECT record_id, record.admin_chat_id, record.shop_chat_id, wx_msg_id,
-	CASE
-	 WHEN admin_chat_id = from_chat_id THEN 1
-	 ELSE 2
-	END AS direction, ytdw.yt_date_format(wx_msg_time, 'yyyy-MM-dd HH:mm:ss') AS msg_time, msg_type,
-	content, extend, room_id, is_qw
-FROM (
-	SELECT *, 1 as tuse_tag
-	FROM channels
-	WHERE chat_source = 2
-) record
-left  JOIN tuse_chats ON
-
--- ((admin_chat_id = from_chat_id AND shop_chat_id = to_chat_id) )
- ((admin_chat_id = to_chat_id AND shop_chat_id = from_chat_id))
-WHERE  tuse_chats.wx_msg_time >= chat_start_time
-AND tuse_chats.wx_msg_time <= chat_end_time
-and tuse_chats.tuse_tag is not null
-and from_chat_id is not null
-and to_chat_id is not null
+             from dw_afs_order_refund_new_d
+             where dayid='$v_date'
+             and refund_status=9
+             group by order_id,refund_status
+	 ) afs_refund on order.order_id = afs_refund.order_id
+                    group by shop_id,shop_pro_id,
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_user_id'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_user_id'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_user_id')),
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_user_name'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_user_name'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_user_name')),
+                        coalesce(ytdw.get_service_info('service_feature_id:5',service_info_freezed,'service_department_name'),ytdw.get_service_info('service_feature_id:1',service_info_freezed,'service_department_name'),ytdw.get_service_info('service_feature_id:4',service_info_freezed,'service_department_name')),
+                        service_info_freezed,
+                        substr(pay_time,1,8)
+                ) tmp1
+                    --省份达标线
+                    left join
+                (
+                    select * from ytdw.ads_salary_base_pro_mark_line_d where dayid='$v_date'
+                ) pro_mark_line on pro_mark_line.shop_pro_id=tmp1.shop_pro_id
+            where pure_pay_amount>=pro_mark_line.new_sign_shop_mark_line
+        ) tmp2 where rn=1
+                 and substr(tmp2.pay_day,1,6)='$v_cur_month' --20200728
+) table2
+    inner join (--20200505
+    select shop_id,
+           shop_name,
+           shop_pro_name,
+           shop_city_name,
+           shop_area_name,
+           store_type_name,
+           approve_status_name
+    from ytdw.dw_shop_base_d where dayid='$v_date' and bu_id=0 and nvl(store_type,100) not in (3,9,10,11) and shop_status in (1,2,3,4,5)
+) table1 on table2.shop_id=table1.shop_id
+--首次下b
+    left join
+(
+    select
+        shop_id,
+        min(substr(pay_time,1,8)) min_pay_day
+    from ytdw.dw_order_d
+    where dayid='$v_date'
+      and pay_time is not null
+      and bu_id=0
+      and sp_id is null
+      --排除员工店、伙伴店11 20200505
+      and nvl(store_type,100) not in (3,9,10,11)
+      and business_unit not in ('卡券票','其他')
+      --B类
+      and item_style_freezed=1
+      and sale_dc_id=-1
+    group by shop_id
+) table3 on table3.shop_id=table1.shop_id
+where substr(table3.min_pay_day,1,6)='$v_cur_month'
