0d94032a08a4bdb41162b721d6b9b07b999ba8b6
桃 <>
2023-04-19 16:48:02 +0800

    云上版本gmv正向明细

--- 有用的/sql/特殊提成/3、模板逻辑表/3.1、存量gmv表/2、明细表/正向/云上版本dw_salary_gmv_rule_forward_public_d.sql
+++ 有用的/sql/特殊提成/3、模板逻辑表/3.1、存量gmv表/2、明细表/正向/云上版本dw_salary_gmv_rule_forward_public_d.sql
@@ -1,5 +1,6 @@
 with plan as (
     SELECT *,
+           1 as tag,
            get_json_object(get_json_object(filter_config_json,'$.calculate_date'),'$.value') as calculate_date_value,
            get_json_object(get_json_object(filter_config_json,'$.calculate_date'),'$.operator') as calculate_date_operator,
            get_json_object(get_json_object(filter_config_json,'$.freeze_sales_team'),'$.value') as freeze_sales_team_value,
@@ -46,7 +47,8 @@ shop_group_mapping as (
 ),
 
 ord as (
-    SELECT order_id,
+    SELECT 1 as tag,
+           order_id,
            pay_day,
            business_unit,
            category_id_first,
@@ -133,7 +135,7 @@ user_admin as (
     where dayid='${v_date}'
 ),
 
-cur as (
+before_cur as (
     SELECT from_unixtime(unix_timestamp(),'yyyy-MM-dd HH:mm:ss') as update_time,
            from_unixtime(unix_timestamp(),'yyyy-MM') as update_month,
            '当月方案' as plan_type,
@@ -266,7 +268,7 @@ cur as (
                'hi_recharge_gmv_less_refund', ord.hi_recharge_gmv_less_refund
            )) as extra
     FROM plan
-    CROSS JOIN ord ON 1 = 1
+    CROSS JOIN ord ON plan.tag = ord.tag
     LEFT JOIN big_bd_manager ON ytdw.get_service_info('service_job_name:大BD',ord.service_info_freezed,'service_department_id') = big_bd_manager.dept_id
     where ord.pay_day between calculate_date_value_start and calculate_date_value_end
     and ytdw.simple_expr(ord.sale_team_freezed_id, 'in', freeze_sales_team_value) = (case when freeze_sales_team_operator = '=' then 1 else 0 end)
@@ -279,8 +281,71 @@ cur as (
     and ytdw.simple_expr(area_manager_dep_id, 'in', bd_area_value) = (case when bd_area_operator = '=' then 1 else 0 end)
     and ytdw.simple_expr(bd_manager_dep_id, 'in', manage_area_value) = (case when manage_area_operator = '=' then 1 else 0 end)
     and if(ord.shop_group = '' OR plan.shop_group_value = '', 0, ytdw.simple_expr(substr(plan.shop_group_value, 2, length(plan.shop_group_value) - 2), 'in', concat('[', ord.shop_group, ']'))) = (case when shop_group_operator ='=' then 1 else 0 end)
-    HAVING ytdw.simple_expr(grant_object_user_id, 'in', filter_user_value) = (case when filter_user_operator = '=' then 1 else 0 end)
-       AND ytdw.simple_expr(grant_object_user_dep_id, 'in', dept_value) = (case when dept_operator = '=' then 1 else 0 end)
+),
+
+cur as (
+    SELECT update_time,
+           update_month,
+           plan_type,
+           plan_month,
+           plan_pay_time,
+           plan_name,
+           plan_group_id,
+           plan_group_name,
+           business_unit,
+           category_id_first,
+           category_id_second,
+           category_id_first_name,
+           category_id_second_name,
+           brand_id,
+           brand_name,
+           item_id,
+           item_name,
+           item_style,
+           item_style_name,
+           is_sp_shop,
+           is_bigbd_shop,
+           is_spec_order,
+           shop_id,
+           shop_name,
+           store_type,
+           store_type_name,
+           war_zone_id,
+           war_zone_name,
+           war_zone_dep_id,
+           war_zone_dep_name,
+           area_manager_id,
+           area_manager_name,
+           area_manager_dep_id,
+           area_manager_dep_name,
+           bd_manager_id,
+           bd_manager_name,
+           bd_manager_dep_id,
+           bd_manager_dep_name,
+           sp_id,
+           sp_name,
+           sp_operator_name,
+           service_user_names_freezed,
+           service_feature_names_freezed,
+           service_job_names_freezed,
+           service_department_names_freezed,
+           service_info_freezed,
+           service_info,
+           grant_object_type,
+           grant_object_user_id,
+           grant_object_user_name,
+           grant_object_user_dep_id,
+           grant_object_user_dep_name,
+           user_admin.leave_time as leave_time,
+           sts_target_name,
+           sts_target,
+           pay_day,
+           planno,
+           extra
+    FROM before_cur
+    INNER JOIN plan ON before_cur.planno = plan.no
+    WHERE ytdw.simple_expr(grant_object_user_id, 'in', filter_user_value) = (case when filter_user_operator = '=' then 1 else 0 end)
+    AND ytdw.simple_expr(grant_object_user_dep_id, 'in', dept_value) = (case when dept_operator = '=' then 1 else 0 end)
 )
 
 insert overwrite table dw_salary_gmv_rule_public_d partition (dayid='${v_date}',pltype='cur')
