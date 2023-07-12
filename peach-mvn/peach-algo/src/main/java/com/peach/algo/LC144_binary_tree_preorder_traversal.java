b06b02264c6b94b6bdeaa2ac6bc5a6dd2877b3df
桃 <>
2023-07-12 14:23:04 +0800

    LC144_binary_tree_preorder_traversal

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC144_binary_tree_preorder_traversal.java
@@ -0,0 +1,43 @@
+package com.peach.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author feitao.zt
+ * @date 2023/7/12
+ */
+public class LC144_binary_tree_preorder_traversal {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        readNode(root, result);
+        return result;
+    }
+
+    private void readNode(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        readNode(root.left, result);
+        readNode(root.right, result);
+    }
+}
