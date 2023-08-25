607fa5f4633f99e130f9591006259665a8503ed3
桃 <>
2023-08-25 16:44:02 +0800

    LC169_majority_element

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC169_majority_element.java
@@ -0,0 +1,23 @@
+package com.peach.algo;
+
+import java.util.Arrays;
+
+/**
+ * @author feitao.zt
+ * @date 2023/8/25
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 示例 1：
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * 示例 2：
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ */
+public class LC169_majority_element {
+
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
