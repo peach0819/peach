c56fd580ce392c7674e6c60f21b61ba9e4525f3f
桃 <>
2023-08-25 11:32:52 +0800

    触达商品ddl

--- 有用的/需求记录/43、CRM8月日常合集/ddl.sql
+++ 有用的/需求记录/43、CRM8月日常合集/ddl.sql
@@ -4,7 +4,7 @@ create table t_touch_task_item (
   editor varchar(32) NOT NULL COMMENT '编辑人',
   create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   edit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  is_deleted tinyint(4) COMMENT '是否删除:0-否 1-是',
+  is_deleted tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除:0-否 1-是',
   task_id bigint(32) NOT NULL COMMENT '触达任务id',
   task_content_id bigint(32) NOT NULL COMMENT '触达内容id',
   item_id bigint(32) NOT NULL COMMENT '商品id',
