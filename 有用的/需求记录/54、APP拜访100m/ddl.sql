bfbf298b56e937496870c332200cb26413137ce8
桃 <tao.zheng8833@hipac.cn>
2024-06-19 10:32:59 +0800

    ddl.sql

--- 有用的/需求记录/54、APP拜访100m/ddl.sql
+++ 有用的/需求记录/54、APP拜访100m/ddl.sql
@@ -1,1 +1,2 @@
 ALTER TABLE sync_ads_crm_shop_reward_brand_d ADD COLUMN dayid varchar(8) COMMENT '数仓表区块标示';
+ALTER TABLE sync_ads_crm_shop_reward_brand_d ADD COLUMN data_month varchar(8) COMMENT '数据月份';
