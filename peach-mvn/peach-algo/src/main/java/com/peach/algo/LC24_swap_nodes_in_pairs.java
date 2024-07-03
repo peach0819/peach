ecf1366db1d24cbb83712e7135a1634e4de72782
桃 <tao.zheng8833@hipac.cn>
2024-07-03 10:41:52 +0800

    LC24_swap_nodes_in_pairs

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC24_swap_nodes_in_pairs.java
@@ -0,0 +1,56 @@
+package com.peach.algo;
+
+/**
+ * @author feitao.zt
+ * @date 2024/7/3
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ * 示例 1：
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ * 输入：head = [1]
+ * 输出：[1]
+ * 提示：
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ */
+public class LC24_swap_nodes_in_pairs {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) { this.val = val; }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = new ListNode(0, head);
+        ListNode index = pre;
+
+        ListNode first;
+        ListNode second;
+        while (index.next != null && index.next.next != null) {
+            first = index.next;
+            second = index.next.next;
+            index.next = second;
+            first.next = second.next;
+            second.next = first;
+            index = first;
+        }
+        return pre.next;
+    }
+}
