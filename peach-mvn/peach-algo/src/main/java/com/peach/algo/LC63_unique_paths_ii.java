896601fcc9cc2a3471d252f51e15422a1a03b5da
桃 <tao.zheng8833@hipac.cn>
2024-07-16 14:18:56 +0800

    LC63_unique_paths_ii

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC63_unique_paths_ii.java
@@ -0,0 +1,55 @@
+package com.peach.algo;
+
+/**
+ * @author feitao.zt
+ * @date 2024/7/16
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * 示例 1：
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * 解释：3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * 示例 2：
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ * 提示：
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] 为 0 或 1
+ */
+public class LC63_unique_paths_ii {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int grid = obstacleGrid[i][j];
+                if (grid == 1) {
+                    result[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    result[i][j] = 1;
+                } else if (i == 0) {
+                    result[i][j] = result[i][j - 1];
+                } else if (j == 0) {
+                    result[i][j] = result[i - 1][j];
+                } else {
+                    result[i][j] = result[i - 1][j] + result[i][j - 1];
+                }
+            }
+        }
+        return result[m - 1][n - 1];
+    }
+}
