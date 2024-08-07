8ccfcfa2425d8efabfe8406d37ea67825f43b0c4
桃 <tao.zheng8833@hipac.cn>
2024-08-07 15:34:21 +0800

    LC164_maximum_gap

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC164_maximum_gap.java
@@ -0,0 +1,65 @@
+package com.peach.algo;
+
+import java.util.Arrays;
+
+/**
+ * @author feitao.zt
+ * @date 2024/8/7
+ * 给定一个无序的数组 nums，返回 数组在排序之后，相邻元素之间最大的差值 。如果数组元素个数小于 2，则返回 0 。
+ * 您必须编写一个在「线性时间」内运行并使用「线性额外空间」的算法。
+ * 示例 1:
+ * 输入: nums = [3,6,9,1]
+ * 输出: 3
+ * 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+ * 示例 2:
+ * 输入: nums = [10]
+ * 输出: 0
+ * 解释: 数组元素个数小于 2，因此返回 0。
+ * 提示:
+ * 1 <= nums.length <= 105
+ * 0 <= nums[i] <= 109
+ */
+public class LC164_maximum_gap {
+
+    /**
+     * 桶排序算法 我是傻逼
+     */
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        int minVal = Arrays.stream(nums).min().getAsInt();
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int d = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketSize = (maxVal - minVal) / d + 1;
+
+        int[][] bucket = new int[bucketSize][2];
+        for (int i = 0; i < bucketSize; ++i) {
+            Arrays.fill(bucket[i], -1); // 存储 (桶内最小值，桶内最大值) 对， (-1, -1) 表示该桶是空的
+        }
+        for (int i = 0; i < n; i++) {
+            int idx = (nums[i] - minVal) / d;
+            if (bucket[idx][0] == -1) {
+                bucket[idx][0] = bucket[idx][1] = nums[i];
+            } else {
+                bucket[idx][0] = Math.min(bucket[idx][0], nums[i]);
+                bucket[idx][1] = Math.max(bucket[idx][1], nums[i]);
+            }
+        }
+
+        int ret = 0;
+        int prev = -1;
+        for (int i = 0; i < bucketSize; i++) {
+            if (bucket[i][0] == -1) {
+                continue;
+            }
+            if (prev != -1) {
+                ret = Math.max(ret, bucket[i][0] - bucket[prev][1]);
+            }
+            prev = i;
+        }
+        return ret;
+    }
+
+}
