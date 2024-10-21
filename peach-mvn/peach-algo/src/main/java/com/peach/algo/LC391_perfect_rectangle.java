f6cfc6d2d6a943adefb3bb38d5b58177bd3cb4c4
桃 <tao.zheng8833@hipac.cn>
2024-10-21 14:29:42 +0800

    LC391_perfect_rectangle

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC391_perfect_rectangle.java
@@ -0,0 +1,106 @@
+package com.peach.algo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author feitao.zt
+ * @date 2024/10/21
+ * 给你一个数组 rectangles ，其中 rectangles[i] = [xi, yi, ai, bi] 表示一个坐标轴平行的矩形。这个矩形的左下顶点是 (xi, yi) ，右上顶点是 (ai, bi) 。
+ * 如果所有矩形一起精确覆盖了某个矩形区域，则返回 true ；否则，返回 false 。
+ * 示例 1：
+ * 输入：rectangles = [[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]
+ * 输出：true
+ * 解释：5 个矩形一起可以精确地覆盖一个矩形区域。
+ * 示例 2：
+ * 输入：rectangles = [[1,1,2,3],[1,3,2,4],[3,1,4,2],[3,2,4,4]]
+ * 输出：false
+ * 解释：两个矩形之间有间隔，无法覆盖成一个矩形。
+ * 示例 3：
+ * 输入：rectangles = [[1,1,3,3],[3,1,4,2],[1,3,2,4],[2,2,4,4]]
+ * 输出：false
+ * 解释：因为中间有相交区域，虽然形成了矩形，但不是精确覆盖。
+ * 提示：
+ * 1 <= rectangles.length <= 2 * 104
+ * rectangles[i].length == 4
+ * -105 <= xi < ai <= 105
+ * -105 <= yi < bi <= 105
+ */
+public class LC391_perfect_rectangle {
+
+    /**
+     * 我是傻逼
+     */
+    public boolean isRectangleCover(int[][] rectangles) {
+        long area = 0;
+        int minX = rectangles[0][0], minY = rectangles[0][1], maxX = rectangles[0][2], maxY = rectangles[0][3];
+        Map<Point, Integer> cnt = new HashMap<>();
+        for (int[] rect : rectangles) {
+            int x = rect[0], y = rect[1], a = rect[2], b = rect[3];
+            area += (long) (a - x) * (b - y);
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, a);
+            maxY = Math.max(maxY, b);
+
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x, b);
+            Point point3 = new Point(a, y);
+            Point point4 = new Point(a, b);
+
+            cnt.put(point1, cnt.getOrDefault(point1, 0) + 1);
+            cnt.put(point2, cnt.getOrDefault(point2, 0) + 1);
+            cnt.put(point3, cnt.getOrDefault(point3, 0) + 1);
+            cnt.put(point4, cnt.getOrDefault(point4, 0) + 1);
+        }
+
+        Point pointMinMin = new Point(minX, minY);
+        Point pointMinMax = new Point(minX, maxY);
+        Point pointMaxMin = new Point(maxX, minY);
+        Point pointMaxMax = new Point(maxX, maxY);
+        if (area != (long) (maxX - minX) * (maxY - minY) || cnt.getOrDefault(pointMinMin, 0) != 1
+                || cnt.getOrDefault(pointMinMax, 0) != 1 || cnt.getOrDefault(pointMaxMin, 0) != 1
+                || cnt.getOrDefault(pointMaxMax, 0) != 1) {
+            return false;
+        }
+
+        cnt.remove(pointMinMin);
+        cnt.remove(pointMinMax);
+        cnt.remove(pointMaxMin);
+        cnt.remove(pointMaxMax);
+
+        for (Map.Entry<Point, Integer> entry : cnt.entrySet()) {
+            int value = entry.getValue();
+            if (value != 2 && value != 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    class Point {
+
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * x + y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Point) {
+                Point point2 = (Point) obj;
+                return this.x == point2.x && this.y == point2.y;
+            }
+            return false;
+        }
+    }
+}
