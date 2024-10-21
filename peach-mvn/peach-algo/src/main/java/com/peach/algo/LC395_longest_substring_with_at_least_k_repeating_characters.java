3619e2eccc96718de011f7a3dfb2f6ae367b3052
桃 <tao.zheng8833@hipac.cn>
2024-10-21 16:59:50 +0800

    LC395_longest_substring_with_at_least_k_repeating_characters

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC395_longest_substring_with_at_least_k_repeating_characters.java
@@ -0,0 +1,49 @@
+package com.peach.algo;
+
+/**
+ * @author feitao.zt
+ * @date 2024/10/21
+ * 给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k 。返回这一子串的长度。
+ * 如果不存在这样的子字符串，则返回 0。
+ * 示例 1：
+ * 输入：s = "aaabb", k = 3
+ * 输出：3
+ * 解释：最长子串为 "aaa" ，其中 'a' 重复了 3 次。
+ * 示例 2：
+ * 输入：s = "ababbc", k = 2
+ * 输出：5
+ * 解释：最长子串为 "ababb" ，其中 'a' 重复了 2 次， 'b' 重复了 3 次。
+ * 提示：
+ * 1 <= s.length <= 104
+ * s 仅由小写英文字母组成
+ * 1 <= k <= 105
+ */
+public class LC395_longest_substring_with_at_least_k_repeating_characters {
+
+    /**
+     * 我是傻逼
+     * 思路是先统计，然后判断小于k的数一定没法被包含，包含了就一定成不了，所以splitBy这些字母
+     */
+    public int longestSubstring(String s, int k) {
+        if (s.length() < k) {
+            return 0;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 0) {
+                continue;
+            }
+            if (counter[i] < k) {
+                int res = 0;
+                for (String t : s.split(String.valueOf((char) (i + 'a')))) {
+                    res = Math.max(res, longestSubstring(t, k));
+                }
+                return res;
+            }
+        }
+        return s.length();
+    }
+}
