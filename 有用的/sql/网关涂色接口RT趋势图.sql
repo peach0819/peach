bcff01bf30ab10317d34bafe90d9cf33e56df392
桃 <tao.zheng8833@hipac.cn>
2024-11-01 10:19:12 +0800

    ref

--- 有用的/sql/网关涂色接口RT趋势图.sql
+++ 有用的/sql/网关涂色接口RT趋势图.sql
@@ -1,4 +1,4 @@
 
 
 
-TUSEQIWEI09  and    transTypeId: TUSEQIWEI09180208| select time_series(__time__, '1m', '%H:%i:%s' ,'0') as Time, avg(prossTime) as UV group by Time order by Time limit 100
+TUSEQIWEI09  and    transTypeId: TUSEQIWEI09180208| select time_series(__time__, '1m', '%H:%i:%s' ,'0') as Time, avg(prossTime) as UV group by Time order by Time limit 10000
