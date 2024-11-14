0c7471190795b59601eb9b2dc3b0f6166a00e4fa
桃 <tao.zheng8833@hipac.cn>
2024-11-14 15:08:39 +0800

    ddl.sql

--- 有用的/需求记录/59、品牌私有化部署（美赞成）/ddl.sql
+++ 有用的/需求记录/59、品牌私有化部署（美赞成）/ddl.sql
@@ -87,6 +87,23 @@ CREATE TABLE `t_crm_visit_indicator_visible` (
   KEY `idx_job_id` (`job_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='拜访指标表';
 
+--目标看板回流表
+CREATE TABLE sync_crm_visit_user_indicator_d (
+  id bigint(16) NOT NULL AUTO_INCREMENT,
+  creator varchar(32) NOT NULL COMMENT '创建人',
+  editor varchar(32) NOT NULL COMMENT '编辑人',
+  create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  edit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  is_deleted tinyint(4) DEFAULT '0' COMMENT '是否删除:0-否 1-是',
+  dayid varchar(8) NOT NULL COMMENT '数仓表区块标示',
+  data_month varchar(100) NOT NULL COMMENT '目标月份',
+  user_id varchar(200) COMMENT '用户id',
+  tab_type tinyint(20) COMMENT '指标类型， 0我自己的 1我团队的 2我下属的',
+  biz_value text COMMENT '业务数据json',
+  PRIMARY KEY (`id`),
+  KEY `idx_data_month_tab_user` (`data_month`, `tab_type`, `user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='目标看板回流表';
+
 ------------------------------------------------------    拜访模版    ---------------------------------------------------
 --拜访模版表
 CREATE TABLE `t_crm_visit_template` (
