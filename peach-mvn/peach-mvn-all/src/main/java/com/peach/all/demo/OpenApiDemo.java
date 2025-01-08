07e5afddcecb8d901801a532bb2d1a6dcb1028e1
桃 <tao.zheng8833@hipac.cn>
2025-01-08 11:36:11 +0800

    openApi更新

--- peach-mvn/peach-mvn-all/src/main/java/com/peach/all/demo/OpenApiDemo.java
+++ peach-mvn/peach-mvn-all/src/main/java/com/peach/all/demo/OpenApiDemo.java
@@ -1,23 +1,9 @@
 package com.peach.all.demo;
 
 import com.alibaba.fastjson.JSON;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import com.google.common.collect.Lists;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,113 +12,14 @@ import java.util.Map;
  */
 public class OpenApiDemo {
 
-    private static final String APP_KEY = "HIPAC91";
-    private static final String SECRET = "11c80b2461aed9c663ea89d144784634";
-    private static final String API = "hipac.crm.shopdanone.elnBrandAuditSyncPush"; //api
-    private static final String ENV = "master"; //测试环境master , 正式环境prod
-    private static final String HOST = "https://master-openapi.hipac.cn"; //测试环境master-openapi , 正式环境openapi
-
     public static void main(String[] args) {
-        try {
-            ElnBrandAuditForm form = new ElnBrandAuditForm();
-            form.setBizType(4);
-            form.setElnStoreCode("100057784");
-            form.setSignBrand("Lingyi");
-            form.setAuditSuccess(true);
-            String result = syncPush(JSON.toJSONString(form));
-            System.out.println("结果：" + result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("接口调用异常：" + e.getMessage());
-        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("versionId", 7);
+        map.put("isLastPage", false);
+        String json = "{\"photoMonth\":\"202212\",\"photoUrl\":\"https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/71845458d6c64e98b3ca7663abddaf32.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/50f99a429985407d80b68936c6c582d9.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/83463960f03d4a96b2bfe93ba4db7189.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/14ff9ef3a6b14b3ebbc1f4b8bb35b98a.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/74c44d1c1f7d4270a189a80959511e6c.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/36de51ceba2445adb82ee38f02987ff5.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/2aa5e807fa8948a791251b6bc98e8564.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/704b8dc884654dd185a73af6430387ed.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/16578c88402c483c9ba24beb82e6d8da.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/52b69f4312be4c56b596bd14790d0cde.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/051a5ddc8ab940f9b99b4dabaa10544e.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/a79a430155ac4a0b95634240756d5671.jpg,https://wyeth-r2c.oss-cn-hangzhou.aliyuncs.com/prod/29e098e2682448a883a5e2cd25da401a.jpg\",\"elnStoreCode\":\"120099122\",\"itemList\":[{\"skuName\":\"AP3\",\"num\":108},{\"skuName\":\"NC3\",\"num\":108},{\"skuName\":\"AP1MINI\",\"num\":108},{\"skuName\":\"AP2\",\"num\":108},{\"skuName\":\"AP1\",\"num\":108},{\"skuName\":\"AC3\",\"num\":108},{\"skuName\":\"NC Tin 2\",\"num\":108},{\"skuName\":\"AC4\",\"num\":108},{\"skuName\":\"NC Tin 3\",\"num\":108},{\"skuName\":\"AC2\",\"num\":108},{\"skuName\":\"NC Tin 1\",\"num\":108},{\"skuName\":\"AP2MINI\",\"num\":108},{\"skuName\":\"AC1\",\"num\":108},{\"skuName\":\"YH 3\",\"num\":108}],\"photoTime\":\"2023-01-31 15:37:31\"}";
+        map.put("dataList", Lists.newArrayList(JSON.parseObject(json)));
+
+        OpenApiUtil.syncPush("hipac.crm.shopdanonesync.syncSpcPhoto", JSON.toJSONString(map), OpenApiUtil.Env.MASTER);
     }
 
-    public static String syncPush(String data) {
-        //第一步 .构建参数
-        Map<String, String> paramMap = buildParamsMap(data);
-
-        //第二步.接口调用
-        return HttpUtil.doPost(HOST, paramMap);
-    }
-
-    private static Map<String, String> buildParamsMap(String data) {
-        Map<String, String> paramMap = new HashMap<>();
-        paramMap.put("route", ENV);
-        paramMap.put("api", API);
-        paramMap.put("appKey", APP_KEY);
-        paramMap.put("v", "1.0.0");
-        paramMap.put("format", "json");
-        paramMap.put("signMethod", "md5");
-        paramMap.put("data", data);
-        String signContent = getSignContent(paramMap);
-        String signed = MD5Util.getMD5(MD5Util.getMD5(signContent) + SECRET);
-        paramMap.put("sign", signed);
-        return paramMap;
-    }
-
-    private static String getSignContent(Map<String, String> paramMap) {
-        StringBuilder content = new StringBuilder();
-        List<String> keys = new ArrayList<>(paramMap.keySet());
-        Collections.sort(keys);
-        for (String key : keys) {
-            String value = paramMap.get(key);
-            if (StringUtils.isNotEmpty(key) && StringUtils.isNotEmpty(value)) {
-                content.append(key).append(value);
-            }
-        }
-        content.append(SECRET);
-        return content.toString();
-    }
-
-    static class HttpUtil {
-
-        /**
-         * post 方式(推荐)
-         */
-        private static String doPost(String url, Map<String, String> map) {
-            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                HttpPost httpPost = new HttpPost(url);
-                //设置参数
-                List<NameValuePair> list = new ArrayList<>();
-                for (Map.Entry<String, String> entry : map.entrySet()) {
-                    if (StringUtils.isNotBlank(entry.getKey()) && StringUtils.isNotBlank(entry.getValue())) {
-                        list.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-                    }
-                }
-                if (list.size() > 0) {
-                    httpPost.setEntity(new UrlEncodedFormEntity(list, "utf-8"));
-                }
-                HttpResponse response = httpClient.execute(httpPost);
-                if (response != null) {
-                    HttpEntity resEntity = response.getEntity();
-                    if (resEntity != null) {
-                        return EntityUtils.toString(resEntity, "utf-8");
-                    }
-                }
-                return null;
-            } catch (Throwable e) {
-                e.printStackTrace();
-                System.out.println("接口调用异常，问题:" + e.getMessage());
-                return null;
-            }
-        }
-    }
-
-    static class MD5Util {
-
-        public static String getMD5(String input) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                byte[] digest = md.digest(input.getBytes());
-
-                StringBuilder sb = new StringBuilder();
-                for (byte b : digest) {
-                    sb.append(String.format("%02x", b & 0xff));
-                }
-                return sb.toString();
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
