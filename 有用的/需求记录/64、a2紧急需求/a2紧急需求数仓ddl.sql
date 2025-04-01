c989369ef6ab02ed973f41f6eff142cbc68cc4ef
桃 <tao.zheng8833@hipac.cn>
2025-04-01 16:40:25 +0800

    ref

--- 有用的/需求记录/64、a2紧急需求/a2紧急需求数仓ddl.sql
+++ 有用的/需求记录/64、a2紧急需求/a2紧急需求数仓ddl.sql
@@ -85,11 +85,11 @@ CREATE TABLE IF NOT EXISTS ads_crm_a2_subject_d
     subject_month   STRING COMMENT '项目月份',
     subject_start_time STRING COMMENT '项目开始时间',
     subject_end_time STRING COMMENT '项目结束时间',
+    shop_type       INT COMMENT '项目门店类型 1机会门店 2优质门店',
     shop_count      INT COMMENT '机会门店数',
     visit_shop_count INT COMMENT '覆盖门店数',
     valid_visit_shop_count INT COMMENT '有效覆盖门店数',
     order_shop_count INT COMMENT '转化门店数',
-    hpc_quality_shop_count INT COMMENT 'HPC优质门店数',
     visit_shop_rate decimal(18, 2) COMMENT '拜访覆盖率'
 ) PARTITIONED BY (dayid STRING);
 
--- 有用的/需求记录/64、a2紧急需求/sql集合/4、ads_crm_a2_subject_d.sql
+++ 有用的/需求记录/64、a2紧急需求/sql集合/4、ads_crm_a2_subject_d.sql
@@ -7,7 +7,7 @@ with subject as (
            subject_month,
            subject_start_time,
            subject_end_time,
-           1 as tag
+           shop_type
     FROM yt_crm.ads_crm_a2_subject_base_d
     WHERE dayid = '${v_date}'
     AND need_stats = 1
@@ -22,15 +22,6 @@ subject_shop as (
     FROM yt_crm.ads_crm_a2_subject_shop_base_d
     WHERE dayid = '${v_date}'
     group by subject_id
-),
-
-month_quality_shop as (
-    SELECT count(case when if_hpc_quality_shop = 1 then 1 else null end) as quality_shop_count,
-           1 as tag
-    FROM yt_bi.ads_hpc_quality_shop_detail_d
-    WHERE dayid = DATE_FORMAT(DATE_SUB(CURRENT_DATE, 1), 'yyyyMMdd')
-    AND data_month = substr('${v_date}', 1, 6)
-    AND shop_prov_id IN (14, 18, 20)
 )
 
 INSERT OVERWRITE TABLE ads_crm_a2_subject_d partition(dayid = '${v_date}')
@@ -45,12 +36,11 @@ SELECT subject.subject_id,
        subject.subject_month,
        subject.subject_start_time,
        subject.subject_end_time,
+       subject.shop_type,
        nvl(subject_shop.shop_count, 0) as shop_count,
        nvl(subject_shop.visit_shop_count, 0) as visit_shop_count,
        nvl(subject_shop.valid_visit_shop_count, 0) as valid_visit_shop_count,
        nvl(subject_shop.order_shop_count, 0) as order_shop_count,
-       nvl(month_quality_shop.quality_shop_count, 0) as quality_shop_count,
        if(nvl(subject_shop.shop_count, 0) = 0, 0, round(100 * nvl(subject_shop.visit_shop_count, 0) /nvl(subject_shop.shop_count, 0), 1)) as visit_shop_rate
 FROM subject
 LEFT JOIN subject_shop ON subject.subject_id = subject_shop.subject_id
-LEFT JOIN month_quality_shop ON subject.tag = month_quality_shop.tag
