3cae87acb008d0537a07aed6d2fb474815eb4397
桃 <tao.zheng8833@hipac.cn>
2025-04-01 11:40:06 +0800

    ref

--- 有用的/需求记录/64、a2紧急需求/sql集合/0、ads_crm_a2_subject_scope_d.sql
+++ 有用的/需求记录/64、a2紧急需求/sql集合/0、ads_crm_a2_subject_scope_d.sql
@@ -1,2 +1,180 @@
 INSERT OVERWRITE TABLE ads_crm_a2_subject_scope_d PARTITION (dayid = '${v_date}')
-SELECT
+SELECT 11290 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11291 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11287 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11295 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11296 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11292 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11297 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11299 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11301 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11303 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11305 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11307 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11309 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11311 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11313 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11315 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11317 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11319 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11321 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11323 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11325 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11327 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11329 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11331 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11333 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11335 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11337 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11339 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11341 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11343 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11345 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11347 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11349 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11351 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11353 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11354 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11355 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11356 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11357 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11358 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11359 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11360 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11361 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11362 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11363 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11364 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11365 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11366 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11367 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11368 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11369 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11370 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11371 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11372 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11373 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11374 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11375 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11376 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11377 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11378 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11379 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11380 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11381 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11382 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11383 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11384 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11385 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11386 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11387 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11388 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11389 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11390 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11391 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11392 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11393 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11394 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11395 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11396 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11397 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11398 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11399 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11400 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11401 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11402 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11403 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11404 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11405 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11406 as subject_id, 0 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11407 as subject_id, 1 as need_stats, 1 as shop_type
+UNION ALL
+SELECT 11408 as subject_id, 1 as need_stats, 1 as shop_type
