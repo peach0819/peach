e2b11dca01a88a723523a8467e0b8e66434932b9
桃 <tao.zheng8833@hipac.cn>
2025-04-18 18:25:52 +0800

    ref

--- 有用的/需求记录/65、美赞1、5、3/sql集合/ads_crm_visit_user_indicator_v2_d.sql
+++ 有用的/需求记录/65、美赞1、5、3/sql集合/ads_crm_visit_user_indicator_v2_d.sql
@@ -8,46 +8,55 @@ with indicator as (
            month_visit_valid_cnt,
            visit_m_target,
            month_visit_valid_rate,
+           month_visit_valid_rate_qualified,
 
            --NKA专职NC门店拜访达成率
            month_nka_nc_visit_valid_cnt,
            month_nka_sever_obj_m,
            month_nka_nc_visit_valid_rate,
+           month_nka_nc_visit_valid_rate_qualified,
 
            --RKA专职NC门店拜访达成率
            month_rka_nc_visit_valid_cnt,
            month_rka_sever_obj_m,
            month_rka_nc_visit_valid_rate,
+           month_rka_nc_visit_valid_rate_qualified,
 
            --院线店拜访达成率
            month_hospital_visit_valid_cnt,
            month_hospital_sever_obj_m,
            month_hospital_visit_valid_rate,
+           month_hospital_visit_valid_rate_qualified,
 
            --门店拜访覆盖率
            month_shop_visit_valid_cnt,
            month_sever_obj_m,
            month_shop_visit_valid_rate,
+           month_shop_visit_valid_rate_qualified,
 
            --月度服务商拜访达成率
            month_fws_visit_valid_cnt,
            month_fws_sever_obj_m,
            month_fws_visit_valid_rate,
+           month_fws_visit_valid_rate_qualified,
 
            --季度服务商拜访达成率
            quar_fws_visit_valid_cnt,
            quar_fws_sever_obj_m,
            quar_fws_visit_valid_rate,
+           quar_fws_visit_valid_rate_qualified,
 
            --月度GT渠道门店拜访覆盖率
            month_gt_shop_visit_valid_cnt,
            month_gt_sever_obj_m,
            month_gt_shop_visit_valid_rate,
+           month_gt_shop_visit_valid_rate_qualified,
 
            --季度GT渠道门店拜访覆盖率
            quar_gt_shop_visit_valid_cnt,
            quar_gt_sever_obj_m,
-           quar_gt_shop_visit_valid_rate
+           quar_gt_shop_visit_valid_rate,
+           quar_gt_shop_visit_valid_rate_qualified
     FROM p_mdson.ads_mdson_user_new_visit_summary_data_d
     WHERE dayid = '${v_date}'
 ),
@@ -101,55 +110,55 @@ SELECT indicator.data_month as data_month,
            'month_visit_freq_valid_rate', month_visit_valid_rate * 100,
            'month_visit_freq_valid_rate_numerator', month_visit_valid_cnt,
            'month_visit_freq_valid_rate_denominator', visit_m_target,
-           'month_visit_freq_valid_rate_reach', '达标',
+           'month_visit_freq_valid_rate_reach', month_visit_valid_rate_qualified,
 
            --NKA专职NC门店拜访达成率
            'month_nka_nc_visit_valid_rate', month_nka_nc_visit_valid_rate * 100,
            'month_nka_nc_visit_valid_rate_numerator', month_nka_nc_visit_valid_cnt,
            'month_nka_nc_visit_valid_rate_denominator', month_nka_sever_obj_m,
-           'month_nka_nc_visit_valid_rate_reach', '达标',
+           'month_nka_nc_visit_valid_rate_reach', month_nka_nc_visit_valid_rate_qualified,
 
           --RKA专职NC门店拜访达成率
            'month_rka_nc_visit_valid_rate', month_rka_nc_visit_valid_rate * 100,
            'month_rka_nc_visit_valid_rate_numerator', month_rka_nc_visit_valid_cnt,
            'month_rka_nc_visit_valid_rate_denominator', month_rka_sever_obj_m,
-           'month_rka_nc_visit_valid_rate_reach', '达标',
+           'month_rka_nc_visit_valid_rate_reach', month_rka_nc_visit_valid_rate_qualified,
 
            --院线店拜访达成率
            'month_hospital_visit_valid_rate', month_hospital_visit_valid_rate * 100,
            'month_hospital_visit_valid_rate_numerator', month_hospital_visit_valid_cnt,
            'month_hospital_visit_valid_rate_denominator', month_hospital_sever_obj_m,
-           'month_hospital_visit_valid_rate_reach', '达标',
+           'month_hospital_visit_valid_rate_reach', month_hospital_visit_valid_rate_qualified,
 
            --门店拜访覆盖率
            'month_shop_visit_valid_rate', month_shop_visit_valid_rate * 100,
            'month_shop_visit_valid_rate_numerator', month_shop_visit_valid_cnt,
            'month_shop_visit_valid_rate_denominator', month_sever_obj_m,
-           'month_shop_visit_valid_rate_reach', '达标',
+           'month_shop_visit_valid_rate_reach', month_shop_visit_valid_rate_qualified,
 
            --月度服务商拜访达成率
            'month_fws_visit_valid_rate', month_fws_visit_valid_rate * 100,
            'month_fws_visit_valid_rate_numerator', month_fws_visit_valid_cnt,
            'month_fws_visit_valid_rate_denominator', month_fws_sever_obj_m,
-           'month_fws_visit_valid_rate_reach', '达标',
+           'month_fws_visit_valid_rate_reach', month_fws_visit_valid_rate_qualified,
 
            --季度服务商拜访达成率
            'quar_fws_visit_valid_rate', quar_fws_visit_valid_rate * 100,
            'quar_fws_visit_valid_rate_numerator', quar_fws_visit_valid_cnt,
            'quar_fws_visit_valid_rate_denominator', quar_fws_sever_obj_m,
-           'quar_fws_visit_valid_rate_reach', '达标',
+           'quar_fws_visit_valid_rate_reach', quar_fws_visit_valid_rate_qualified,
 
            --月度GT渠道门店拜访覆盖率
            'month_gt_shop_visit_valid_rate', month_gt_shop_visit_valid_rate * 100,
            'month_gt_shop_visit_valid_rate_numerator', month_gt_shop_visit_valid_cnt,
            'month_gt_shop_visit_valid_rate_denominator', month_gt_sever_obj_m,
-           'month_gt_shop_visit_valid_rate_reach', '达标',
+           'month_gt_shop_visit_valid_rate_reach', month_gt_shop_visit_valid_rate_qualified,
 
            --季度GT渠道门店拜访覆盖率
            'quar_gt_shop_visit_valid_rate', quar_gt_shop_visit_valid_rate * 100,
            'quar_gt_shop_visit_valid_rate_numerator', quar_gt_shop_visit_valid_cnt,
            'quar_gt_shop_visit_valid_rate_denominator', quar_gt_sever_obj_m,
-           'quar_gt_shop_visit_valid_rate_reach', '达标'
+           'quar_gt_shop_visit_valid_rate_reach', quar_gt_shop_visit_valid_rate_qualified
        )) as biz_value
 FROM user
 INNER JOIN indicator ON indicator.user_id = user.user_id
@@ -232,7 +241,7 @@ SELECT /*+ mapjoin(user) */
        user.user_id,
        2 as tab_type,
        to_json(named_struct(
-                      'user_cnt', count(sub.user_id),
+           'user_cnt', count(sub.user_id),
 
            --当月拜访人员达标率
            'month_visit_reach_rate', round(100 * count(case when if_visit_qualified = '达标' then 1 else null end)/count(sub.user_id), 2),
