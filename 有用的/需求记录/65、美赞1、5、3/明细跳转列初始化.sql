2c498f7d5249b9e21fac704d5d96f9d291cce2c4
桃 <tao.zheng8833@hipac.cn>
2025-04-18 17:27:12 +0800

    ref

--- 有用的/需求记录/65、美赞1、5、3/明细跳转列初始化.sql
+++ 有用的/需求记录/65、美赞1、5、3/明细跳转列初始化.sql
@@ -1,14 +1,4 @@
 
-INSERT INTO t_crm_visit_indicator(`id`, `creator`, `editor`, `indicator_code`, `indicator_name`, `indicator_unit`, `indicator_type`, `indicator_display_type`)
-VALUES (120, 'system', 'system', 'month_visit_freq_valid_rate', '门店拜访频次达成率', '%', 1, 'ratio'),
-       (121, 'system', 'system', 'month_nka_nc_visit_valid_rate', 'NKA专职NC门店拜访达成率', '%', 1, 'ratio'),
-       (122, 'system', 'system', 'month_rka_nc_visit_valid_rate', 'RKA专职NC门店拜访达成率', '%', 1, 'ratio'),
-       (123, 'system', 'system', 'month_hospital_visit_valid_rate', '院线店拜访达成率', '%', 1, 'ratio'),
-       (124, 'system', 'system', 'month_shop_visit_valid_rate', '门店拜访覆盖率', '%', 1, 'ratio'),
-       (125, 'system', 'system', 'month_fws_visit_valid_rate', '月度服务商拜访达成率', '%', 1, 'ratio'),
-       (126, 'system', 'system', 'quar_fws_visit_valid_rate', '季度服务商拜访达成率', '%', 1, 'ratio'),
-       (127, 'system', 'system', 'month_gt_shop_visit_valid_rate', '月度GT渠道门店拜访覆盖率', '%', 1, 'ratio'),
-       (128, 'system', 'system', 'quar_gt_shop_visit_valid_rate', '季度GT渠道门店拜访覆盖率', '%', 1, 'ratio');
 
 --当月拜访人员达标率
 UPDATE t_crm_visit_indicator
