1b5989e41c052156ad58a38e64e14f00398ca445
桃 <tao.zheng8833@hipac.cn>
2025-05-12 15:10:36 +0800

    ref

--- 有用的/sql/特殊提成/3、模板逻辑表/3.2、新签商品表/3、汇总表/正向/云上dw_salary_sign_item_rule_forward_plan_sum_d.sql
+++ 有用的/sql/特殊提成/3、模板逻辑表/3.2、新签商品表/3、汇总表/正向/云上dw_salary_sign_item_rule_forward_plan_sum_d.sql
@@ -46,7 +46,7 @@ detail as (
 
 underling as (
     select user_id, max(underling_cnt) as underling_cnt
-    from ytdw.dws_usr_bd_manager_underling_d
+    from ytdw.dws_usr_bd_manager_underling_v2_d
     where dayid ='${v_date}'
     group by user_id
 ),
