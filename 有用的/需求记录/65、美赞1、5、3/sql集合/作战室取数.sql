de66791b3c45b4c43bdc0ac43bd0eccda3898ddb
桃 <tao.zheng8833@hipac.cn>
2025-05-28 11:25:34 +0800

    ref

--- 有用的/需求记录/65、美赞1、5、3/sql集合/作战室取数.sql
+++ 有用的/需求记录/65、美赞1、5、3/sql集合/作战室取数.sql
@@ -1,7 +1,7 @@
 with user as (
     SELECT user_id, user_real_name
     FROM p_mdson.ads_crm_visit_user_d
-    WHERE dayid = '20250520'
+    WHERE dayid = '20250527'
 ),
 
 data as (
@@ -49,7 +49,7 @@ data as (
            get_json_object(biz_value, '$.month_hospital_visit_valid_rate_numerator') as month_hospital_visit_valid_rate_numerator,
            get_json_object(biz_value, '$.month_hospital_visit_valid_rate_denominator') as month_hospital_visit_valid_rate_denominator
     FROM p_mdson.ads_crm_visit_user_indicator_v2_d
-    WHERE dayid = '20250520'
+    WHERE dayid = '20250527'
     AND data_month = '2025-05'
     AND user_id NOT IN ('admin')
 )
@@ -61,41 +61,38 @@ SELECT data.user_id as `用户id`,
        nvl(data.month_visit_reach_rate, '') as `当月拜访人员达标率`,
        nvl(data.month_visit_reach_rate_numerator, '') as `当月拜访人员达标率分子`,
        nvl(data.month_visit_reach_rate_denominator, '') as `当月拜访人员达标率分母`,
-       nvl(data.month_visit_freq_valid_rate, '') as `门店拜访频次达成率`,
-       nvl(data.month_visit_freq_valid_rate_numerator, '') as `门店拜访频次达成率分子`,
-       nvl(data.month_visit_freq_valid_rate_denominator, '') as `门店拜访频次达成率分母`,
-       nvl(data.month_nka_nc_visit_valid_rate, '') as `NKA专职NC门店拜访达成率`,
-       nvl(data.month_nka_nc_visit_valid_rate_numerator, '') as `NKA专职NC门店拜访达成率分子`,
-       nvl(data.month_nka_nc_visit_valid_rate_denominator, '') as `NKA专职NC门店拜访达成率分母`,
-       nvl(data.month_rka_nc_visit_valid_rate, '') as `RKA专职NC门店拜访达成率`,
-       nvl(data.month_rka_nc_visit_valid_rate_numerator, '') as `RKA专职NC门店拜访达成率分子`,
-       nvl(data.month_rka_nc_visit_valid_rate_denominator, '') as `RKA专职NC门店拜访达成率分母`,
-       nvl(data.month_hospital_visit_valid_rate, '') as `院线店拜访达成率`,
-       nvl(data.month_hospital_visit_valid_rate_numerator, '') as `院线店拜访达成率分子`,
-       nvl(data.month_hospital_visit_valid_rate_denominator, '') as `院线店拜访达成率分母`,
-       nvl(data.month_shop_visit_valid_rate, '') as `门店拜访覆盖率`,
-       nvl(data.month_shop_visit_valid_rate_numerator, '') as `门店拜访覆盖率分子`,
-       nvl(data.month_shop_visit_valid_rate_denominator, '') as `门店拜访覆盖率分母`,
-       nvl(data.month_fws_visit_valid_rate, '') as `月度服务商拜访达成率`,
-       nvl(data.month_fws_visit_valid_rate_numerator, '') as `月度服务商拜访达成率分子`,
-       nvl(data.month_fws_visit_valid_rate_denominator, '') as `月度服务商拜访达成率分母`,
-       nvl(data.quar_fws_visit_valid_rate, '') as `季度服务商拜访达成率`,
-       nvl(data.quar_fws_visit_valid_rate_numerator, '') as `季度服务商拜访达成率分子`,
-       nvl(data.quar_fws_visit_valid_rate_denominator, '') as `季度服务商拜访达成率分母`,
-       nvl(data.month_gt_shop_visit_valid_rate, '') as `月度GT渠道门店拜访覆盖率`,
-       nvl(data.month_gt_shop_visit_valid_rate_numerator, '') as `月度GT渠道门店拜访覆盖率分子`,
-       nvl(data.month_gt_shop_visit_valid_rate_denominator, '') as `月度GT渠道门店拜访覆盖率分母`,
-       nvl(data.quar_gt_shop_visit_valid_rate, '') as `季度GT渠道门店拜访覆盖率`,
-       nvl(data.quar_gt_shop_visit_valid_rate_numerator, '') as `季度GT渠道门店拜访覆盖率分子`,
-       nvl(data.quar_gt_shop_visit_valid_rate_denominator, '') as `季度GT渠道门店拜访覆盖率分母`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate, '') as `月度GT渠道院线店拜访覆盖率`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate_numerator, '') as `月度GT渠道院线店拜访覆盖率分子`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate_denominator, '') as `月度GT渠道院线店拜访覆盖率分母`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate, '') as `季度GT渠道院线店拜访覆盖率`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate_numerator, '') as `季度GT渠道院线店拜访覆盖率分子`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate_denominator, '') as `季度GT渠道院线店拜访覆盖率分母`,
-       nvl(data.month_hospital_visit_valid_rate, '') as `院线店拜访覆盖率`,
-       nvl(data.month_hospital_visit_valid_rate_numerator, '') as `院线店拜访覆盖率分子`,
-       nvl(data.month_hospital_visit_valid_rate_denominator, '') as `院线店拜访覆盖率分母`
+       nvl(data.month_visit_freq_valid_rate, '') as `当月门店拜访频次达成率`,
+       nvl(data.month_visit_freq_valid_rate_numerator, '') as `当月门店拜访频次达成率分子`,
+       nvl(data.month_visit_freq_valid_rate_denominator, '') as `当月门店拜访频次达成率分母`,
+       nvl(data.month_nka_nc_visit_valid_rate, '') as `当月NKA专职NC门店拜访达成率`,
+       nvl(data.month_nka_nc_visit_valid_rate_numerator, '') as `当月NKA专职NC门店拜访达成率分子`,
+       nvl(data.month_nka_nc_visit_valid_rate_denominator, '') as `当月NKA专职NC门店拜访达成率分母`,
+       nvl(data.month_rka_nc_visit_valid_rate, '') as `当月RKA专职NC门店拜访达成率`,
+       nvl(data.month_rka_nc_visit_valid_rate_numerator, '') as `当月RKA专职NC门店拜访达成率分子`,
+       nvl(data.month_rka_nc_visit_valid_rate_denominator, '') as `当月RKA专职NC门店拜访达成率分母`,
+       nvl(data.month_shop_visit_valid_rate, '') as `当月门店拜访覆盖率`,
+       nvl(data.month_shop_visit_valid_rate_numerator, '') as `当月门店拜访覆盖率分子`,
+       nvl(data.month_shop_visit_valid_rate_denominator, '') as `当月门店拜访覆盖率分母`,
+       nvl(data.month_fws_visit_valid_rate, '') as `当月服务商拜访达成率`,
+       nvl(data.month_fws_visit_valid_rate_numerator, '') as `当月服务商拜访达成率分子`,
+       nvl(data.month_fws_visit_valid_rate_denominator, '') as `当月服务商拜访达成率分母`,
+       nvl(data.quar_fws_visit_valid_rate, '') as `当季服务商拜访达成率`,
+       nvl(data.quar_fws_visit_valid_rate_numerator, '') as `当季服务商拜访达成率分子`,
+       nvl(data.quar_fws_visit_valid_rate_denominator, '') as `当季服务商拜访达成率分母`,
+       nvl(data.month_gt_shop_visit_valid_rate, '') as `当月GT渠道门店拜访覆盖率`,
+       nvl(data.month_gt_shop_visit_valid_rate_numerator, '') as `当月GT渠道门店拜访覆盖率分子`,
+       nvl(data.month_gt_shop_visit_valid_rate_denominator, '') as `当月GT渠道门店拜访覆盖率分母`,
+       nvl(data.quar_gt_shop_visit_valid_rate, '') as `当季GT渠道门店拜访覆盖率`,
+       nvl(data.quar_gt_shop_visit_valid_rate_numerator, '') as `当季GT渠道门店拜访覆盖率分子`,
+       nvl(data.quar_gt_shop_visit_valid_rate_denominator, '') as `当季GT渠道门店拜访覆盖率分母`,
+       nvl(data.month_gt_hospital_shop_visit_valid_rate, '') as `当月GT渠道院线店拜访覆盖率`,
+       nvl(data.month_gt_hospital_shop_visit_valid_rate_numerator, '') as `当月GT渠道院线店拜访覆盖率分子`,
+       nvl(data.month_gt_hospital_shop_visit_valid_rate_denominator, '') as `当月GT渠道院线店拜访覆盖率分母`,
+       nvl(data.quar_gt_hospital_shop_visit_valid_rate, '') as `当季GT渠道院线店拜访覆盖率`,
+       nvl(data.quar_gt_hospital_shop_visit_valid_rate_numerator, '') as `当季GT渠道院线店拜访覆盖率分子`,
+       nvl(data.quar_gt_hospital_shop_visit_valid_rate_denominator, '') as `当季GT渠道院线店拜访覆盖率分母`,
+       nvl(data.month_hospital_visit_valid_rate, '') as `当月院线店拜访覆盖率`,
+       nvl(data.month_hospital_visit_valid_rate_numerator, '') as `当月院线店拜访覆盖率分子`,
+       nvl(data.month_hospital_visit_valid_rate_denominator, '') as `当月院线店拜访覆盖率分母`
 FROM data
 INNER JOIN user ON data.user_id = user.user_id
