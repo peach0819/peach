c536f75ac184e23623e1e5f3ca53503474eb14cd
桃 <tao.zheng8833@hipac.cn>
2025-06-27 16:12:04 +0800

    LC566_reshape_the_matrix

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC566_reshape_the_matrix.java
@@ -0,0 +1,38 @@
+package com.peach.algo;
+
+/**
+ * @author feitao.zt
+ * @date 2025/6/27
+ * 在 MATLAB 中，有一个非常有用的函数 reshape ，它可以将一个 m x n 矩阵重塑为另一个大小不同（r x c）的新矩阵，但保留其原始数据。
+ * 给你一个由二维数组 mat 表示的 m x n 矩阵，以及两个正整数 r 和 c ，分别表示想要的重构的矩阵的行数和列数。
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的 行遍历顺序 填充。
+ * 如果具有给定参数的 reshape 操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ * 示例 1：
+ * 输入：mat = [[1,2],[3,4]], r = 1, c = 4
+ * 输出：[[1,2,3,4]]
+ * 示例 2：
+ * 输入：mat = [[1,2],[3,4]], r = 2, c = 4
+ * 输出：[[1,2],[3,4]]
+ * 提示：
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 100
+ * -1000 <= mat[i][j] <= 1000
+ * 1 <= r, c <= 300
+ */
+public class LC566_reshape_the_matrix {
+
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if (mat.length * mat[0].length != r * c) {
+            return mat;
+        }
+        int[][] res = new int[r][c];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                int index = i * mat[i].length + j;
+                res[index / c][index % c] = mat[i][j];
+            }
+        }
+        return res;
+    }
+}
