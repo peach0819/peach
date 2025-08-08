5cf4fc9128d1d205d7c35ad2832823ce29fe6b65
桃 <tao.zheng8833@hipac.cn>
2025-08-08 11:13:28 +0800

    ref

--- 有用的/需求记录/65、美赞1、5、3/sql集合/ads_crm_visit_user_d.sql
+++ 有用的/需求记录/65、美赞1、5、3/sql集合/ads_crm_visit_user_d.sql
@@ -23,25 +23,9 @@ root_dept as (
            row_number() over(partition by id) as cnt
     FROM dept
     LATERAL VIEW explode(split(root_key, '_')) dept_id AS dept_id
-)
+),
 
-INSERT OVERWRITE TABLE ads_crm_visit_user_d PARTITION (dayid = '${v_date}')
-SELECT user_id,
-       user_real_name,
-       root_key as user_dept_root_key,
-       if(
-           count(if(user_id != charger, 1, null)) > 0,
-           concat(concat_ws('_' , collect_list(if(user_id != charger, charger, null))), '_', user_id),
-           user_id
-       ) as user_root_key,
-       if(
-           count(if(user_id != charger, 1, null)) > 0,
-           concat(concat_ws('_' , collect_list(if(user_id != charger, charger_name, null))), '_', user_real_name),
-           user_real_name
-       ) as user_name_root_key,
-       concat_ws('_' , collect_list(if(user_id != charger, charger, null))) as user_parent_root_key,
-       concat_ws('_' , collect_list(if(user_id != charger, charger_name, null))) as user_name_parent_root_key
-FROM (
+root_user as (
     SELECT user_admin.user_id,
            user_admin.user_real_name,
            base_dept.root_key,
@@ -54,8 +38,25 @@ FROM (
     LEFT JOIN root_dept ON user_admin.dept_id = root_dept.id
     LEFT JOIN dept ON root_dept.dept_id = dept.id
     LEFT JOIN user_admin charger ON dept.charger = charger.user_id
-    order by cnt
-) root_user
+)
+
+INSERT OVERWRITE TABLE ads_crm_visit_user_d PARTITION (dayid = '${v_date}')
+SELECT user_id,
+       user_real_name,
+       root_key as user_dept_root_key,
+       if(
+           count(if(user_id != charger, 1, null)) > 0,
+           concat(p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc'), '_', user_id),
+           user_id
+       ) as user_root_key,
+       if(
+           count(if(user_id != charger, 1, null)) > 0,
+           concat(p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc'), '_', user_real_name),
+           user_real_name
+       ) as user_name_root_key,
+       p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc') AS user_parent_root_key,
+	   p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc') AS user_name_parent_root_key
+FROM root_user
 group by user_id,
          user_real_name,
          root_key
