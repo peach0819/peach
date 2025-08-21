afc79d8688c65c692ad5d1507371e5c76679890a
桃 <tao.zheng8833@hipac.cn>
2025-08-21 10:10:32 +0800

    ref

--- 有用的/需求记录/67、海拍客小记同步到a2/ddl.sql
+++ 有用的/需求记录/67、海拍客小记同步到a2/ddl.sql
@@ -1,3 +1,3 @@
 --crm_report、a2_core
--- ALTER TABLE sync_crm_a2_subject_shop_base_d ADD COLUMN `last_visit_record_id` bigint(20) COMMENT '最近一次拜访小记id';
+ALTER TABLE sync_crm_a2_subject_shop_base_d ADD COLUMN `last_visit_record_id` bigint(20) COMMENT '最近一次拜访小记id';
 
