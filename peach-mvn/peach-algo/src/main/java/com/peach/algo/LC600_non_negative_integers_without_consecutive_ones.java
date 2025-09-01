80c5efebc3885612efbb0639ddf8799654ab8d22
桃 <tao.zheng8833@hipac.cn>
2025-09-01 10:41:42 +0800

    LC600_non_negative_integers_without_consecutive_ones

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC600_non_negative_integers_without_consecutive_ones.java
@@ -0,0 +1,61 @@
+package com.peach.algo;
+
+/**
+ * @author feitao.zt
+ * @date 2025/8/28
+ * 给定一个正整数 n ，请你统计在 [0, n] 范围的非负整数中，有多少个整数的二进制表示中不存在 连续的 1 。
+ * 示例 1:
+ * 输入: n = 5
+ * 输出: 5
+ * 解释:
+ * 下面列出范围在 [0, 5] 的非负整数与其对应的二进制表示：
+ * 0 : 0
+ * 1 : 1
+ * 2 : 10
+ * 3 : 11
+ * 4 : 100
+ * 5 : 101
+ * 其中，只有整数 3 违反规则（有两个连续的 1 ），其他 5 个满足规则。
+ * 示例 2:
+ * 输入: n = 1
+ * 输出: 2
+ * 示例 3:
+ * 输入: n = 2
+ * 输出: 3
+ * 提示:
+ * 1 <= n <= 109
+ */
+public class LC600_non_negative_integers_without_consecutive_ones {
+
+    /**
+     * 我是傻逼
+     */
+    public int findIntegers(int n) {
+        int[] dp = new int[31];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < 31; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int pre = 0, res = 0;
+        for (int i = 29; i >= 0; --i) {
+            int val = 1 << i;
+            if ((n & val) != 0) {
+                res += dp[i + 1];
+                if (pre == 1) {
+                    break;
+                }
+                pre = 1;
+            } else {
+                pre = 0;
+            }
+
+            if (i == 0) {
+                ++res;
+            }
+        }
+
+        return res;
+    }
+
+}
