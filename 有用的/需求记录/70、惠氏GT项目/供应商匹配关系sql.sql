a99f8a83a129ada79da839df5c9911417de18de4
桃 <tao.zheng8833@hipac.cn>
2025-09-23 10:20:02 +0800

    ref

---
+++ 有用的/需求记录/70、惠氏GT项目/供应商匹配关系sql.sql
@@ -0,0 +1,176 @@
+SELECT t1.*, a.area_id as areaId
+FROM (
+SELECT distinct *
+FROM (
+SELECT '潮州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '东莞市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '佛山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '佛山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '广州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '广州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '广州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '广州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '河源市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '河源市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '惠州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '惠州市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '江门市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '揭阳市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '茂名市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '茂名市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '梅州市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '梅州市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '梅州市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '清远市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '清远市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '清远市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '清远市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '清远市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '汕头市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '汕头市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '汕尾市' as areaName,	'SD4673' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '韶关市' as areaName,	'SD4675' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '深圳市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '深圳市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '深圳市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '深圳市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '深圳市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '阳江市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '阳江市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '云浮市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '云浮市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '湛江市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '湛江市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '湛江市' as areaName,	'SD4672' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '肇庆市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '肇庆市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '肇庆市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '中山市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '珠海市' as areaName,	'SD4676' as distributorCode,	'SD4519' as supplierCode,	'SD4519' as strategyCode
+UNION ALL
+SELECT '滨州市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '德州市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '东营市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '菏泽市' as areaName,	'SD4680' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '济南市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '济宁市' as areaName,	'SD4680' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '聊城市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '青岛市' as areaName,	'SD4679' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '泰安市' as areaName,	'SD4680' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '威海市' as areaName,	'SD4679' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '潍坊市' as areaName,	'SD4681' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '烟台市' as areaName,	'SD4679' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+UNION ALL
+SELECT '淄博市' as areaName,	'SD4680' as distributorCode,	'SD4532' as supplierCode,	'SD4532' as strategyCode
+) t
+) t1
+INNER JOIN yt_ustone.t_area a ON t1.areaName = a.area_name   --过滤东莞
