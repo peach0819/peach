7737ba8e43ca3ff28f2117b94ee87651e01d9286
桃 <tao.zheng8833@hipac.cn>
2025-10-17 14:09:56 +0800

    ref

--- 有用的/sql/p0项目管理-阿里云/通用/ads_crm_subject_feature_group_d.sql
+++ 有用的/sql/p0项目管理-阿里云/通用/ads_crm_subject_feature_group_d.sql
@@ -1,10 +1,38 @@
 INSERT OVERWRITE TABLE ads_crm_subject_feature_group_d partition (dayid='${v_date}')
-select 1 as feature_type,3 AS group_type,'1,5,12,14' AS feature_ids,'\_32\_' as dept_root_key
+--BD团队
+select 1 as feature_type,
+       3 AS group_type,
+       '1,5,12,14' AS feature_ids,
+       '32' as dept_root_key
+
 UNION ALL
-select 1 as feature_type,4 AS group_type,'21' AS feature_ids,'\_1804\_' as dept_root_key
+
+--CBD团队
+select 1 as feature_type,
+       4 AS group_type,
+       '21' AS feature_ids,
+       '1804' as dept_root_key
+
 UNION ALL
-select 2 as feature_type,1 AS group_type,'2,6,7,13,15' AS feature_ids,'\_63\_' as dept_root_key
+
+--电销团队
+select 2 as feature_type,
+       1 AS group_type,
+       '2,6,7,13,15' AS feature_ids,
+       '63' as dept_root_key
+
 UNION ALL
-select 2 as feature_type,2 AS group_type,'40,41' AS feature_ids,'\_2211\_' as dept_root_key
+
+--VS团队
+select 2 as feature_type,
+       2 AS group_type,
+       '40,41' AS feature_ids,
+       '2211' as dept_root_key
+
 UNION ALL
-select 2 as feature_type,5 AS group_type,'68' AS feature_ids,'\_1945\_' as dept_root_key
+
+--综合电销团队
+select 2 as feature_type,
+       5 AS group_type,
+       '68' AS feature_ids,
+       '1945' as dept_root_key
