7a0fd110e73878339b1cae6bb1c4f804aa8ab411
桃 <tao.zheng8833@hipac.cn>
2025-10-21 14:03:26 +0800

    ads_crm_subject_recommend_h

--- 有用的/sql/p0项目管理-阿里云/准实时/ads_crm_subject_recommend_h.sql
+++ 有用的/sql/p0项目管理-阿里云/准实时/ads_crm_subject_recommend_h.sql
@@ -3,68 +3,35 @@
 --@exclude_input=ytdw.ods_vf_pt_order_shop
 --@exclude_input=ytdw.ods_vf_t_p0_subject
 --@exclude_input=ytdw.ods_vf_t_p0_subject_shop
-WITH shop_user as (
-    select *
-    from yt_crm.ads_crm_subject_shop_user_d
-    WHERE dayid = '${v_date}'
+WITH shop_saler as (
+    SELECT shop_id,
+           user_id,
+           dept_id,
+           parent_dept_id,
+           parent_2_dept_id
+    FROM (
+        select *,
+               row_number() over (PARTITION BY shop_id ORDER BY group_type ASC) AS rn
+        from yt_crm.ads_crm_subject_shop_user_d
+        WHERE dayid = '${v_date}'
+        AND feature_type = 2
+    ) shop_user
+    WHERE rn = 1
 ),
 
-shop_saler as (
-	SELECT if(nvl(ts.shop_id,'')='',vs.shop_id,ts.shop_id) as shop_id,
-           ts.user_id AS ts_user_id,
-           ts.dept_id AS ts_dept_id,
-           ts.parent_dept_id AS ts_parent_dept_id,
-           ts.parent_2_dept_id AS ts_parent_2_dept_id,
-           vs.user_id AS vs_user_id,
-           vs.dept_id AS vs_dept_id,
-           vs.parent_dept_id AS vs_parent_dept_id,
-           vs.parent_2_dept_id AS vs_parent_2_dept_id
-    FROM (select * from shop_user WHERE group_type_tag = 'ts') ts
-    FULL JOIN (select * from shop_user WHERE group_type_tag = 'vs') vs on ts.shop_id = vs.shop_id
-)
-
-INSERT OVERWRITE TABLE ads_crm_subject_recommend_h PARTITION (dayid='${v_date}')
-SELECT shop_subject.shop_id,
-       shop.shop_name,
-       nvl(shop.shop_pro_id, 0),
-       nvl(shop.shop_city_id, 0),
-       nvl(shop.shop_area_id, 0),
-       nvl(shop.shop_address_id, 0),
-       nvl(if(shop_saler.ts_user_id != '', shop_saler.ts_user_id, shop_saler.vs_user_id), '') AS user_id,
-       nvl(if(shop_saler.ts_dept_id != 0, shop_saler.ts_dept_id, shop_saler.vs_dept_id), 0) AS dept_id,
-       nvl(if(shop_saler.ts_parent_dept_id != 0, shop_saler.ts_parent_dept_id, shop_saler.vs_parent_dept_id), 0) AS parent_dept_id,
-       nvl(if(shop_saler.ts_parent_2_dept_id != 0, shop_saler.ts_parent_2_dept_id, shop_saler.vs_parent_2_dept_id), 0) AS parent_2_dept_id,
-       nvl(subject_ids, ''),
-       nvl(subject_count, 0),
-       nvl(subject_data, ''),
-       nvl(shop_visit.visit_frequency, ''),
-	   CASE WHEN visit_times > 0 THEN 1 ELSE 0 END AS no_valid_visit,
-	   CASE WHEN invalid_visit_times > 0 THEN 1 ELSE 0 END AS no_invalid_visit,
-       shop_subject.last_visit_time AS last_visit_time,
-       shop_visit.last_visit_time AS last_request_time,
-       shop_pay.pay_time AS last_pay_time
-FROM (
+shop_subject as (
 	SELECT subject_shop.shop_id,
            count(*) AS subject_count,
            concat_ws(',', sort_array(collect_set(CAST(subject_shop.subject_id AS string)))) AS subject_ids,
-           concat(
-               '[',
-                concat_ws(',',
-                    sort_array(
-                        collect_set(
-                            concat(
-                                '{"subject_id":', subject_shop.subject_id,
-                                ',"subject_gmv":', nvl(subject_gmv, 0),
-                                ',"this_month_gmv":', nvl(this_month_gmv, 0),
-                                ',"last_1_month_gmv":', nvl(last_1_month_gmv, 0),
-                                ',"last_2_month_gmv":', nvl(last_2_month_gmv, 0),
-                                ',"valid_visit_times":', nvl(visit_times, 0),
-                                ',"invalid_visit_times":', nvl(invalid_visit_times, 0), '}')
-                            )
-                        )
-                    ),
-                ']'
-            ) AS subject_data,
+           to_json(sort_array(collect_list(NAMED_STRUCT(
+               'subject_id', subject_shop.subject_id,
+               'subject_gmv', nvl(cast(subject_gmv as decimal(18,2)), 0),
+               'this_month_gmv', nvl(cast(this_month_gmv as decimal(18,2)), 0),
+               'last_1_month_gmv', nvl(cast(last_1_month_gmv as decimal(18,2)), 0),
+               'last_2_month_gmv', nvl(cast(last_2_month_gmv as decimal(18,2)), 0),
+               'valid_visit_times', nvl(visit_times, 0),
+               'invalid_visit_times', nvl(invalid_visit_times, 0)
+           )))) AS subject_data,
            sum(visit_times) AS visit_times,
            sum(invalid_visit_times) AS invalid_visit_times,
            max(last_visit_time) AS last_visit_time
@@ -98,17 +65,23 @@ FROM (
 		WHERE dayid = '${v_date}'
 	) subject_shop_gmv ON subject_shop_gmv.subject_id = subject_shop.subject_id AND subject_shop_gmv.shop_id = subject_shop.shop_id
 	GROUP BY subject_shop.shop_id
-) shop_subject
-left JOIN shop_saler ON shop_subject.shop_id = shop_saler.shop_id
-left join (select * FROM ytdw.dw_shop_base_d WHERE dayid='${v_date}') shop on shop.shop_id=shop_subject.shop_id
-LEFT JOIN (
+),
+
+shop as (
+    select *
+    FROM ytdw.dw_shop_base_d
+    WHERE dayid='${v_date}'
+),
+
+shop_visit as (
 	SELECT shop_id,
            visit_frequency,
            last_visit_time
 	FROM yt_crm.nrt_ts_shop_visit
 	WHERE dayid = '${v_date}'
-) shop_visit ON shop_subject.shop_id = shop_visit.shop_id
-LEFT JOIN (
+),
+
+shop_pay as (
 	SELECT shop_id,
            max(pay_time) AS pay_time
 	FROM (
@@ -134,4 +107,30 @@ LEFT JOIN (
 	) sp_order ON all_order.order_id = sp_order.order_id
 	WHERE sp_order.sp_id IS NULL
 	GROUP BY shop_id
-) shop_pay ON shop_subject.shop_id = shop_pay.shop_id
+)
+
+INSERT OVERWRITE TABLE ads_crm_subject_recommend_h PARTITION (dayid='${v_date}')
+SELECT shop_subject.shop_id,
+       shop.shop_name,
+       nvl(shop.shop_pro_id, 0),
+       nvl(shop.shop_city_id, 0),
+       nvl(shop.shop_area_id, 0),
+       nvl(shop.shop_address_id, 0),
+       nvl(shop_saler.user_id, '') AS user_id,
+       nvl(shop_saler.dept_id, 0) AS dept_id,
+       nvl(shop_saler.parent_dept_id, 0) AS parent_dept_id,
+       nvl(shop_saler.parent_2_dept_id, 0) AS parent_2_dept_id,
+       nvl(subject_ids, ''),
+       nvl(subject_count, 0),
+       nvl(subject_data, ''),
+       nvl(shop_visit.visit_frequency, ''),
+	   CASE WHEN visit_times > 0 THEN 1 ELSE 0 END AS no_valid_visit,
+	   CASE WHEN invalid_visit_times > 0 THEN 1 ELSE 0 END AS no_invalid_visit,
+       shop_subject.last_visit_time AS last_visit_time,
+       shop_visit.last_visit_time AS last_request_time,
+       shop_pay.pay_time AS last_pay_time
+FROM shop_subject
+left JOIN shop_saler ON shop_subject.shop_id = shop_saler.shop_id
+left join shop on shop.shop_id=shop_subject.shop_id
+LEFT JOIN shop_visit ON shop_subject.shop_id = shop_visit.shop_id
+LEFT JOIN shop_pay ON shop_subject.shop_id = shop_pay.shop_id
