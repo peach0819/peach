01567418ebb088e27cc3f772d5cb1d90e1fe1498
桃 <tao.zheng8833@hipac.cn>
2026-02-05 15:03:47 +0800

    LC721_accounts_merge

---
+++ peach-mvn/peach-algo/src/main/java/com/peach/algo/LC721_accounts_merge.java
@@ -0,0 +1,100 @@
+package com.peach.algo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * @author feitao.zt
+ * @date 2026/2/5
+ * 给定一个列表 accounts，每个元素 accounts[i] 是一个字符串列表，其中第一个元素 accounts[i][0] 是 名称 (name)，其余元素是 emails 表示该账户的邮箱地址。
+ * 现在，我们想合并这些账户。如果两个账户都有一些共同的邮箱地址，则两个账户必定属于同一个人。请注意，即使两个账户具有相同的名称，它们也可能属于不同的人，因为人们可能具有相同的名称。一个人最初可以拥有任意数量的账户，但其所有账户都具有相同的名称。
+ * 合并账户后，按以下格式返回账户：每个账户的第一个元素是名称，其余元素是 按字符 ASCII 顺序排列 的邮箱地址。账户本身可以以 任意顺序 返回。
+ * 示例 1：
+ * 输入：accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+ * 输出：[["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+ * 解释：
+ * 第一个和第三个 John 是同一个人，因为他们有共同的邮箱地址 "johnsmith@mail.com"。
+ * 第二个 John 和 Mary 是不同的人，因为他们的邮箱地址没有被其他帐户使用。
+ * 可以以任何顺序返回这些列表，例如答案 [['Mary'，'mary@mail.com']，['John'，'johnnybravo@mail.com']，
+ * ['John'，'john00@mail.com'，'john_newyork@mail.com'，'johnsmith@mail.com']] 也是正确的。
+ * 示例 2：
+ * 输入：accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+ * 输出：[["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+ * 提示：
+ * 1 <= accounts.length <= 1000
+ * 2 <= accounts[i].length <= 10
+ * 1 <= accounts[i][j].length <= 30
+ * accounts[i][0] 由英文字母组成
+ * accounts[i][j] (for j > 0) 是有效的邮箱地址
+ */
+public class LC721_accounts_merge {
+
+    public static void main(String[] args) {
+        LC721_accounts_merge accountsMerge = new LC721_accounts_merge();
+        List<List<String>> accounts = new ArrayList<>();
+        accounts.add(new ArrayList<>(Arrays.asList("John", "johnsmith@mail.com", "john00@mail.com")));
+        accounts.add(new ArrayList<>(Arrays.asList("John", "johnnybravo@mail.com")));
+        accounts.add(new ArrayList<>(Arrays.asList("John", "johnsmith@mail.com", "john_newyork@mail.com")));
+        accounts.add(new ArrayList<>(Arrays.asList("Mary", "mary@mail.com")));
+        List<List<String>> lists = accountsMerge.accountsMerge(accounts);
+    }
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, List<List<String>>> map = new HashMap<>();
+        for (List<String> account : accounts) {
+            String name = account.get(0);
+            map.computeIfAbsent(name, k -> new ArrayList<>()).add(new ArrayList<>(account.subList(1, account.size())));
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (String key : map.keySet()) {
+            List<List<String>> merge = merge(map.get(key));
+            for (List<String> strings : merge) {
+                List<String> list = new ArrayList<>();
+                list.add(key);
+                strings.sort(String::compareTo);
+                list.addAll(strings);
+                result.add(list);
+            }
+        }
+        return result;
+    }
+
+    private List<List<String>> merge(List<List<String>> lists) {
+        boolean[] visit = new boolean[lists.size()];
+        List<List<String>> result = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        for (int i = 0; i < lists.size(); i++) {
+            if (visit[i]) {
+                continue;
+            }
+            visit[i] = true;
+            Set<String> cur = new HashSet<>();
+            queue.addAll(lists.get(i));
+            while (!queue.isEmpty()) {
+                String poll = queue.poll();
+                if (cur.contains(poll)) {
+                    continue;
+                }
+                cur.add(poll);
+                for (int j = i + 1; j < lists.size(); j++) {
+                    if (visit[j]) {
+                        continue;
+                    }
+                    if (lists.get(j).contains(poll)) {
+                        visit[j] = true;
+                        queue.addAll(lists.get(j));
+                    }
+                }
+            }
+            result.add(new ArrayList<>(cur));
+        }
+        return result;
+    }
+}
