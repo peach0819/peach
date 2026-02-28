7b237069cadf0106c5708caf5ebd1088ccba89bd
桃 <tao.zheng8833@hipac.cn>
2026-02-28 15:13:31 +0800

    ref

---
+++ 有用的/需求记录/77、项目管理时间数据订正/ddl.sql
@@ -0,0 +1,6 @@
+update t_p0_subject set do_start = '2025-12-01 00:00:00', do_end = '2025-12-31 00:00:00', stat_start = '2025-12-01 00:00:00', stat_end = '2025-12-31 00:00:00', subject_month = '202512'  WHERE id = 13766;
+update t_p0_subject set do_start = '2025-10-01 00:00:00', do_end = '2025-10-31 00:00:00', stat_start = '2025-10-01 00:00:00', stat_end = '2025-10-31 00:00:00', subject_month = '202510'  WHERE id = 13765;
+update t_p0_subject set do_start = '2025-08-23 00:00:00', do_end = '2025-08-31 00:00:00', stat_start = '2025-08-23 00:00:00', stat_end = '2025-08-31 00:00:00', subject_month = '202508'  WHERE id = 13764;
+update t_p0_subject set do_start = '2025-11-01 00:00:00', do_end = '2025-11-30 00:00:00', stat_start = '2025-11-01 00:00:00', stat_end = '2025-11-30 00:00:00', subject_month = '202511'  WHERE id = 13762;
+update t_p0_subject set do_start = '2025-09-01 00:00:00', do_end = '2025-09-30 00:00:00', stat_start = '2025-09-01 00:00:00', stat_end = '2025-09-30 00:00:00', subject_month = '202509'  WHERE id = 13761;
+update t_p0_subject set do_start = '2025-06-01 00:00:00', do_end = '2025-06-30 00:00:00', stat_start = '2025-06-01 00:00:00', stat_end = '2025-06-30 00:00:00', subject_month = '202506'  WHERE id = 13763;
