ce5ce8ce42f013e417a170ef2be943f4de53528b
桃 <tao.zheng8833@hipac.cn>
2026-03-03 14:32:14 +0800

    ref

--- 有用的/需求记录/77、项目管理时间数据订正/ddl.sql
+++ 有用的/需求记录/77、项目管理时间数据订正/ddl.sql
@@ -4,3 +4,16 @@ update t_p0_subject set do_start = '2025-08-23 00:00:00', do_end = '2025-08-31 0
 update t_p0_subject set do_start = '2025-11-01 00:00:00', do_end = '2025-11-30 00:00:00', stat_start = '2025-11-01 00:00:00', stat_end = '2025-11-30 00:00:00', subject_month = '202511'  WHERE id = 13762;
 update t_p0_subject set do_start = '2025-09-01 00:00:00', do_end = '2025-09-30 00:00:00', stat_start = '2025-09-01 00:00:00', stat_end = '2025-09-30 00:00:00', subject_month = '202509'  WHERE id = 13761;
 update t_p0_subject set do_start = '2025-06-01 00:00:00', do_end = '2025-06-30 00:00:00', stat_start = '2025-06-01 00:00:00', stat_end = '2025-06-30 00:00:00', subject_month = '202506'  WHERE id = 13763;
+
+
+update t_p0_subject set edit_time = '2025-12-31 00:00:00'  WHERE id = 13766;
+update t_p0_subject set edit_time = '2025-10-31 00:00:00'  WHERE id = 13765;
+update t_p0_subject set edit_time = '2025-08-31 00:00:00'  WHERE id = 13764;
+update t_p0_subject set edit_time = '2025-11-30 00:00:00'  WHERE id = 13762;
+update t_p0_subject set edit_time = '2025-09-30 00:00:00'  WHERE id = 13761;
+update t_p0_subject set edit_time = '2025-06-30 00:00:00'  WHERE id = 13763;
+
+
+update t_p0_subject_group set root_dept_id = 1958 WHERE id  = 2;
+update t_p0_subject_group set root_dept_id = 2232 WHERE id  = 5;
+
