2c379c0a0245a0fe258b8811bb5e5e9ed097fd67
桃 <tao.zheng8833@hipac.cn>
2026-05-25 16:46:07 +0800

    ref

--- 有用的/需求记录/77、项目管理时间数据订正/ddl.sql
+++ 有用的/需求记录/77、项目管理时间数据订正/ddl.sql
@@ -24,8 +24,8 @@ update t_p0_subject_group set root_dept_id = 2232 WHERE id  = 5;
 14473	可心柔浙江省项目-11.1-11.30
 14474	可心柔浙江省项目-12.1-12.31
 
-update t_p0_subject set do_start = '2025-08-23 00:00:00', do_end = '2025-08-31 00:00:00', stat_start = '2025-08-23 00:00:00', stat_end = '2025-08-31 00:00:00', subject_month = '202508', edit_time = '2025-08-31 00:00:00'  WHERE id = 14470;
-update t_p0_subject set do_start = '2025-09-01 00:00:00', do_end = '2025-09-30 00:00:00', stat_start = '2025-09-01 00:00:00', stat_end = '2025-09-30 00:00:00', subject_month = '202509', edit_time = '2025-09-30 00:00:00'  WHERE id = 14471;
-update t_p0_subject set do_start = '2025-10-01 00:00:00', do_end = '2025-10-31 00:00:00', stat_start = '2025-10-01 00:00:00', stat_end = '2025-10-31 00:00:00', subject_month = '202510', edit_time = '2025-10-31 00:00:00'  WHERE id = 14472;
-update t_p0_subject set do_start = '2025-11-01 00:00:00', do_end = '2025-11-30 00:00:00', stat_start = '2025-11-01 00:00:00', stat_end = '2025-11-30 00:00:00', subject_month = '202511', edit_time = '2025-11-30 00:00:00'  WHERE id = 14473;
-update t_p0_subject set do_start = '2025-12-01 00:00:00', do_end = '2025-12-31 00:00:00', stat_start = '2025-12-01 00:00:00', stat_end = '2025-12-31 00:00:00', subject_month = '202512', edit_time = '2025-12-31 00:00:00'  WHERE id = 14474;
+update t_p0_subject set status = 2, do_start = '2025-08-23 00:00:00', do_end = '2025-08-31 00:00:00', stat_start = '2025-08-23 00:00:00', stat_end = '2025-08-31 00:00:00', subject_month = '202508', edit_time = '2025-08-31 00:00:00'  WHERE id = 14470;
+update t_p0_subject set status = 2, do_start = '2025-09-01 00:00:00', do_end = '2025-09-30 00:00:00', stat_start = '2025-09-01 00:00:00', stat_end = '2025-09-30 00:00:00', subject_month = '202509', edit_time = '2025-09-30 00:00:00'  WHERE id = 14471;
+update t_p0_subject set status = 2, do_start = '2025-10-01 00:00:00', do_end = '2025-10-31 00:00:00', stat_start = '2025-10-01 00:00:00', stat_end = '2025-10-31 00:00:00', subject_month = '202510', edit_time = '2025-10-31 00:00:00'  WHERE id = 14472;
+update t_p0_subject set status = 2, do_start = '2025-11-01 00:00:00', do_end = '2025-11-30 00:00:00', stat_start = '2025-11-01 00:00:00', stat_end = '2025-11-30 00:00:00', subject_month = '202511', edit_time = '2025-11-30 00:00:00'  WHERE id = 14473;
+update t_p0_subject set status = 2, do_start = '2025-12-01 00:00:00', do_end = '2025-12-31 00:00:00', stat_start = '2025-12-01 00:00:00', stat_end = '2025-12-31 00:00:00', subject_month = '202512', edit_time = '2025-12-31 00:00:00'  WHERE id = 14474;
