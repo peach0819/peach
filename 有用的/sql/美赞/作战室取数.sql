b5dfc8ed4d5e26fb748a09e1fb27b113acf79bc5
桃 <tao.zheng8833@hipac.cn>
2026-06-22 18:19:37 +0800

    ref

--- 有用的/sql/美赞/作战室取数.sql
+++ 有用的/sql/美赞/作战室取数.sql
@@ -1,98 +1,52 @@
-with user as (
-    SELECT user_id, user_real_name
-    FROM p_mdson.ads_crm_visit_user_d
-    WHERE dayid = '20251204'
-),
-
-data as (
-    SELECT user_id,
-           case tab_type when 0 then '个人的'
-                         when 1 then '团队的（包含本人和下级）'
-                         when 2 then '下级的（不包含本人）'
-                         when 4 then '区域前线'
-                         end as tab_type,
-           get_json_object(biz_value, '$.month_visit_my_reach') as month_visit_my_reach,
-           get_json_object(biz_value, '$.month_visit_reach_rate') as month_visit_reach_rate,
-           get_json_object(biz_value, '$.month_visit_reach_rate_numerator') as month_visit_reach_rate_numerator,
-           get_json_object(biz_value, '$.month_visit_reach_rate_denominator') as month_visit_reach_rate_denominator,
-           get_json_object(biz_value, '$.month_visit_freq_valid_rate') as month_visit_freq_valid_rate,
-           get_json_object(biz_value, '$.month_visit_freq_valid_rate_numerator') as month_visit_freq_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_visit_freq_valid_rate_denominator') as month_visit_freq_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_nka_nc_visit_valid_rate') as month_nka_nc_visit_valid_rate,
-           get_json_object(biz_value, '$.month_nka_nc_visit_valid_rate_numerator') as month_nka_nc_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_nka_nc_visit_valid_rate_denominator') as month_nka_nc_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_rka_nc_visit_valid_rate') as month_rka_nc_visit_valid_rate,
-           get_json_object(biz_value, '$.month_rka_nc_visit_valid_rate_numerator') as month_rka_nc_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_rka_nc_visit_valid_rate_denominator') as month_rka_nc_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_shop_visit_valid_rate') as month_shop_visit_valid_rate,
-           get_json_object(biz_value, '$.month_shop_visit_valid_rate_numerator') as month_shop_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_shop_visit_valid_rate_denominator') as month_shop_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_fws_visit_valid_rate') as month_fws_visit_valid_rate,
-           get_json_object(biz_value, '$.month_fws_visit_valid_rate_numerator') as month_fws_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_fws_visit_valid_rate_denominator') as month_fws_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.quar_fws_visit_valid_rate') as quar_fws_visit_valid_rate,
-           get_json_object(biz_value, '$.quar_fws_visit_valid_rate_numerator') as quar_fws_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.quar_fws_visit_valid_rate_denominator') as quar_fws_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_gt_shop_visit_valid_rate') as month_gt_shop_visit_valid_rate,
-           get_json_object(biz_value, '$.month_gt_shop_visit_valid_rate_numerator') as month_gt_shop_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_gt_shop_visit_valid_rate_denominator') as month_gt_shop_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.quar_gt_shop_visit_valid_rate') as quar_gt_shop_visit_valid_rate,
-           get_json_object(biz_value, '$.quar_gt_shop_visit_valid_rate_numerator') as quar_gt_shop_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.quar_gt_shop_visit_valid_rate_denominator') as quar_gt_shop_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_gt_hospital_shop_visit_valid_rate') as month_gt_hospital_shop_visit_valid_rate,
-           get_json_object(biz_value, '$.month_gt_hospital_shop_visit_valid_rate_numerator') as month_gt_hospital_shop_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_gt_hospital_shop_visit_valid_rate_denominator') as month_gt_hospital_shop_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.quar_gt_hospital_shop_visit_valid_rate') as quar_gt_hospital_shop_visit_valid_rate,
-           get_json_object(biz_value, '$.quar_gt_hospital_shop_visit_valid_rate_numerator') as quar_gt_hospital_shop_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.quar_gt_hospital_shop_visit_valid_rate_denominator') as quar_gt_hospital_shop_visit_valid_rate_denominator,
-           get_json_object(biz_value, '$.month_hospital_visit_valid_rate') as month_hospital_visit_valid_rate,
-           get_json_object(biz_value, '$.month_hospital_visit_valid_rate_numerator') as month_hospital_visit_valid_rate_numerator,
-           get_json_object(biz_value, '$.month_hospital_visit_valid_rate_denominator') as month_hospital_visit_valid_rate_denominator
-    FROM p_mdson.ads_crm_visit_user_indicator_v2_d
-    WHERE dayid = '20251204'
-    AND data_month = '2025-12'
-    AND user_id NOT IN ('admin')
-)
-
 SELECT data.user_id as `用户id`,
        user.user_real_name as `用户名`,
        data.tab_type as `指标类型`,
-       nvl(data.month_visit_my_reach, '') as `当月我的拜访达标`,
-       nvl(data.month_visit_reach_rate, '') as `当月拜访人员达标率`,
-       nvl(data.month_visit_reach_rate_numerator, '') as `当月拜访人员达标率分子`,
-       nvl(data.month_visit_reach_rate_denominator, '') as `当月拜访人员达标率分母`,
-       nvl(data.month_visit_freq_valid_rate, '') as `当月门店拜访频次达成率`,
-       nvl(data.month_visit_freq_valid_rate_numerator, '') as `当月门店拜访频次达成率分子`,
-       nvl(data.month_visit_freq_valid_rate_denominator, '') as `当月门店拜访频次达成率分母`,
-       nvl(data.month_nka_nc_visit_valid_rate, '') as `当月NKA专职NC门店拜访达成率`,
-       nvl(data.month_nka_nc_visit_valid_rate_numerator, '') as `当月NKA专职NC门店拜访达成率分子`,
-       nvl(data.month_nka_nc_visit_valid_rate_denominator, '') as `当月NKA专职NC门店拜访达成率分母`,
-       nvl(data.month_rka_nc_visit_valid_rate, '') as `当月RKA专职NC门店拜访达成率`,
-       nvl(data.month_rka_nc_visit_valid_rate_numerator, '') as `当月RKA专职NC门店拜访达成率分子`,
-       nvl(data.month_rka_nc_visit_valid_rate_denominator, '') as `当月RKA专职NC门店拜访达成率分母`,
-       nvl(data.month_shop_visit_valid_rate, '') as `当月门店拜访覆盖率`,
-       nvl(data.month_shop_visit_valid_rate_numerator, '') as `当月门店拜访覆盖率分子`,
-       nvl(data.month_shop_visit_valid_rate_denominator, '') as `当月门店拜访覆盖率分母`,
-       nvl(data.month_fws_visit_valid_rate, '') as `当月服务商拜访达成率`,
-       nvl(data.month_fws_visit_valid_rate_numerator, '') as `当月服务商拜访达成率分子`,
-       nvl(data.month_fws_visit_valid_rate_denominator, '') as `当月服务商拜访达成率分母`,
-       nvl(data.quar_fws_visit_valid_rate, '') as `当季服务商拜访达成率`,
-       nvl(data.quar_fws_visit_valid_rate_numerator, '') as `当季服务商拜访达成率分子`,
-       nvl(data.quar_fws_visit_valid_rate_denominator, '') as `当季服务商拜访达成率分母`,
-       nvl(data.month_gt_shop_visit_valid_rate, '') as `当月GT渠道门店拜访覆盖率`,
-       nvl(data.month_gt_shop_visit_valid_rate_numerator, '') as `当月GT渠道门店拜访覆盖率分子`,
-       nvl(data.month_gt_shop_visit_valid_rate_denominator, '') as `当月GT渠道门店拜访覆盖率分母`,
-       nvl(data.quar_gt_shop_visit_valid_rate, '') as `当季GT渠道门店拜访覆盖率`,
-       nvl(data.quar_gt_shop_visit_valid_rate_numerator, '') as `当季GT渠道门店拜访覆盖率分子`,
-       nvl(data.quar_gt_shop_visit_valid_rate_denominator, '') as `当季GT渠道门店拜访覆盖率分母`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate, '') as `当月GT渠道院线店拜访覆盖率`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate_numerator, '') as `当月GT渠道院线店拜访覆盖率分子`,
-       nvl(data.month_gt_hospital_shop_visit_valid_rate_denominator, '') as `当月GT渠道院线店拜访覆盖率分母`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate, '') as `当季GT渠道院线店拜访覆盖率`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate_numerator, '') as `当季GT渠道院线店拜访覆盖率分子`,
-       nvl(data.quar_gt_hospital_shop_visit_valid_rate_denominator, '') as `当季GT渠道院线店拜访覆盖率分母`,
-       nvl(data.month_hospital_visit_valid_rate, '') as `当月院线店拜访覆盖率`,
-       nvl(data.month_hospital_visit_valid_rate_numerator, '') as `当月院线店拜访覆盖率分子`,
-       nvl(data.month_hospital_visit_valid_rate_denominator, '') as `当月院线店拜访覆盖率分母`
-FROM data
-INNER JOIN user ON data.user_id = user.user_id
+
+       nvl(get_json_object(biz_value, '$.month_visit_my_reach'), '') as `当月我的拜访达标`,
+       nvl(get_json_object(biz_value, '$.month_visit_reach_rate'), '') as `当月拜访人员达标率`,
+       nvl(get_json_object(biz_value, '$.month_visit_reach_rate_numerator'), '') as `当月拜访人员达标率分子`,
+       nvl(get_json_object(biz_value, '$.month_visit_reach_rate_denominator'), '') as `当月拜访人员达标率分母`,
+       nvl(get_json_object(biz_value, '$.quarter_visit_my_reach'), '') as `当季我的拜访达标`,
+
+	   nvl(get_json_object(biz_value, '$.month_visit_freq_reach_rate'), '')  as `当月拜访频次达标率`,
+	   nvl(get_json_object(biz_value, '$.month_visit_freq_reach_rate_numerator'), '')  as `当月拜访频次达标率分子`,
+	   nvl(get_json_object(biz_value, '$.month_visit_freq_reach_rate_denominator'), '')  as `当月拜访频次达标率分母`,
+	   nvl(get_json_object(biz_value, '$.month_nc_visit_reach_rate'), '')  as `当月专职NC门店拜访达成率`,
+	   nvl(get_json_object(biz_value, '$.month_nc_visit_reach_rate_numerator'), '')  as `当月专职NC门店拜访达成率分子`,
+	   nvl(get_json_object(biz_value, '$.month_nc_visit_reach_rate_denominator'), '')  as `当月专职NC门店拜访达成率分母`,
+	   nvl(get_json_object(biz_value, '$.month_fws_visit_cover_rate'), '')  as `当月服务商拜访达成率`,
+	   nvl(get_json_object(biz_value, '$.month_fws_visit_cover_rate_numerator'), '')  as `当月服务商拜访达成率分子`,
+	   nvl(get_json_object(biz_value, '$.month_fws_visit_cover_rate_denominator'), '')  as `当月服务商拜访达成率分母`,
+	   nvl(get_json_object(biz_value, '$.quarter_fws_visit_cover_rate'), '')  as `当季服务商拜访覆盖率`,
+	   nvl(get_json_object(biz_value, '$.quarter_fws_visit_cover_rate_numerator'), '')  as `当季服务商拜访覆盖率分子`,
+	   nvl(get_json_object(biz_value, '$.quarter_fws_visit_cover_rate_denominator'), '')  as `当季服务商拜访覆盖率分母`,
+	   nvl(get_json_object(biz_value, '$.month_star_visit_reach_rate'), '')  as `当月星级门店拜访达成率`,
+	   nvl(get_json_object(biz_value, '$.month_star_visit_reach_rate_numerator'), '')  as `当月星级门店拜访达成率分子`,
+	   nvl(get_json_object(biz_value, '$.month_star_visit_reach_rate_denominator'), '')  as `当月星级门店拜访达成率分母`,
+	   nvl(get_json_object(biz_value, '$.month_shop_visit_reach_rate'), '')  as `当月门店拜访达成率`,
+	   nvl(get_json_object(biz_value, '$.month_shop_visit_reach_rate_numerator'), '')  as `当月门店拜访达成率分子`,
+	   nvl(get_json_object(biz_value, '$.month_shop_visit_reach_rate_denominator'), '')  as `当月门店拜访达成率分母`,
+	   nvl(get_json_object(biz_value, '$.month_all_big_visit_cover_rate'), '')  as `当季全渠道重点门店拜访覆盖率`,
+	   nvl(get_json_object(biz_value, '$.month_all_big_visit_cover_rate_numerator'), '')  as `当季全渠道重点门店拜访覆盖率分子`,
+	   nvl(get_json_object(biz_value, '$.month_all_big_visit_cover_rate_denominator'), '')  as `当季全渠道重点门店拜访覆盖率分母`,
+	   nvl(get_json_object(biz_value, '$.month_hospital_visit_reach_rate'), '')  as `当月院线店拜访达成率`,
+	   nvl(get_json_object(biz_value, '$.month_hospital_visit_reach_rate_numerator'), '')  as `当月院线店拜访达成率分子`,
+	   nvl(get_json_object(biz_value, '$.month_hospital_visit_reach_rate_denominator'), '')  as `当月院线店拜访达成率分母`
+FROM (
+    SELECT user_id,
+           case tab_type when 0 then '个人的'
+                         when 2 then '下级的（不包含本人）'
+                         when 4 then '区域前线'
+                         end as tab_type,
+           biz_value
+    FROM prod_mdson.ads_crm_visit_user_indicator_d  --取的灰度表
+    WHERE dayid = '${v_date}'
+    AND data_month = concat(substr('${v_date}', 1, 4), '-', substr('${v_date}', 5, 2))
+    AND user_id NOT IN ('admin')
+    AND tab_type IN (0, 2, 4)
+) data
+INNER JOIN (
+    SELECT user_id, user_real_name
+    FROM prod_mdson.ads_crm_visit_user_d
+    WHERE dayid = '${v_date}'
+) user ON data.user_id = user.user_id
--- 有用的/sql/美赞/作战室明细取数.sql
+++ 有用的/sql/美赞/作战室明细取数.sql
@@ -1,9 +1,29 @@
 SELECT user_id,
-       indicator_code,
-       service_obj_id,
-       service_obj_name,
-       GET_JSON_OBJECT(biz_value, '$.indicator') as reach
-       GET_JSON_OBJECT(biz_value, '$.reach') as reach
-FROM p_mdson.ads_crm_visit_user_indicator_detail_d
-WHERE dayid = '20251204'
+       case indicator_code when month_visit_reach_rate then '当月我的拜访达标'
+                           when month_visit_freq_reach_rate then '当月拜访频次达标率'
+                           when month_nc_visit_reach_rate then '当月专职NC门店拜访达成率'
+                           when month_fws_visit_cover_rate then '当月服务商拜访达成率'
+                           when quarter_fws_visit_cover_rate then '当季服务商拜访覆盖率'
+                           when month_star_visit_reach_rate then '当月星级门店拜访达成率'
+                           when month_shop_visit_reach_rate then '当月门店拜访达成率'
+                           when month_all_big_visit_cover_rate then '当季全渠道重点门店拜访覆盖率'
+                           when month_hospital_visit_reach_rate then '当月院线店拜访达成率'
+                           end as `指标名称`,
+       service_obj_id as `拜访对象id`,
+       service_obj_name as `拜访对象名`,
+       GET_JSON_OBJECT(biz_value, '$.indicator') as `指标值`,
+       GET_JSON_OBJECT(biz_value, '$.reach') as `是否达标`
+FROM prod_mdson.ads_crm_visit_user_indicator_detail_d
+WHERE dayid = '${v_date}'
 AND indicator_code is not null
+AND indicator_code IN (
+    'month_visit_reach_rate',
+    'month_visit_freq_reach_rate',
+    'month_nc_visit_reach_rate',
+    'month_fws_visit_cover_rate',
+    'quarter_fws_visit_cover_rate',
+    'month_star_visit_reach_rate',
+    'month_shop_visit_reach_rate',
+    'month_all_big_visit_cover_rate',
+    'month_hospital_visit_reach_rate'
+)
