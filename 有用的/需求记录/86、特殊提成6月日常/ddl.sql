dcb1c0f1a697bee33c7f526eaeae265b687eb32c
桃 <tao.zheng8833@hipac.cn>
2026-06-24 11:39:24 +0800

    ref

--- 有用的/需求记录/86、特殊提成6月日常/ddl.sql
+++ 有用的/需求记录/86、特殊提成6月日常/ddl.sql
@@ -6,8 +6,7 @@ INSERT INTO `bounty_filter` (`id`, `title`, `key`, `field_sql`, `required`, `glo
 VALUES (35, '冻结二级销售团队标识', 'freeze_second_sales_team', 'freeze_second_sales_team', 1, 1, 1, 0, 'enum_multi_select', '{"type":"url", "url":"hipac.datacenter.filterContent.queryFilterContentList","code":"second_sale_team"}');
 
 insert into `bounty_rule_filter` (`bounty_rule_type`,`bounty_rule_type_id`,`bounty_filter_id`)
-values (0, 4, 35),
-       (0, 6, 35);
+values (0, 4, 35);
 
 --新增业务组（财务口径）
 INSERT INTO `bounty_filter` (`id`, `title`, `key`, `field_sql`, `required`, `global`, `global_type`, `plan_date_sign`, `component_type`, `component_value_config`)
@@ -16,3 +15,10 @@ VALUES (36, '业务组(财务口径)', 'order_group_name', 'order_group_name', 0
 insert into `bounty_rule_filter` (`bounty_rule_type`,`bounty_rule_type_id`,`bounty_filter_id`)
 values (0, 4, 36),
        (0, 6, 36);
+
+--新增二级销售团队标识
+INSERT INTO `bounty_filter` (`id`, `title`, `key`, `field_sql`, `required`, `global`, `global_type`, `plan_date_sign`, `component_type`, `component_value_config`)
+VALUES (37, '二级销售团队标识', 'second_sales_team', 'second_sales_team', 1, 1, 1, 0, 'enum_multi_select', '{"type":"url", "url":"hipac.datacenter.filterContent.queryFilterContentList","code":"second_sale_team"}');
+
+insert into `bounty_rule_filter` (`bounty_rule_type`,`bounty_rule_type_id`,`bounty_filter_id`)
+values (0, 6, 37);
