ab2f1cc3529a5ea0fb2d174a5d34d15f69240706
桃 <tao.zheng8833@hipac.cn>
2026-07-06 11:12:34 +0800

    ref

--- 有用的/sql/美赞/0、基础数据/ads_crm_visit_user_d.sql
+++ 有用的/sql/美赞/0、基础数据/ads_crm_visit_user_d.sql
@@ -2,7 +2,7 @@ with user_admin as (
     SELECT user_id,
            user_real_name,
            dept_id
-    FROM p_mdson.dim_user_d
+    FROM dim_user_d
     WHERE dayid = '${v_date}'
     AND user_status = 1
 ),
@@ -11,18 +11,18 @@ dept as (
     SELECT id,
            concat(root_key, id) as root_key,
            charger
-    FROM p_mdson.dwd_department_d
+    FROM dwd_department_d
     WHERE dayid = '${v_date}'
 ),
 
 root_dept as (
-    SELECT id,
+      SELECT id,
            root_key,
            dept_id,
            charger,
-           row_number() over(partition by id) as cnt
+           cnt + 1 as cnt
     FROM dept
-    LATERAL VIEW explode(split(root_key, '_')) dept_id AS dept_id
+    LATERAL VIEW posexplode(split(root_key, '_')) pos as cnt, dept_id
 ),
 
 root_user as (
@@ -46,16 +46,16 @@ SELECT user_id,
        root_key as user_dept_root_key,
        if(
            count(if(user_id != charger, 1, null)) > 0,
-           concat(p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc'), '_', user_id),
+           concat(prod_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc'), '_', user_id),
            user_id
        ) as user_root_key,
        if(
            count(if(user_id != charger, 1, null)) > 0,
-           concat(p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc'), '_', user_real_name),
+           concat(prod_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc'), '_', user_real_name),
            user_real_name
        ) as user_name_root_key,
-       p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc') AS user_parent_root_key,
-	   p_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc') AS user_name_parent_root_key
+       prod_mdson.MapValueSorter(map_agg(if(user_id != charger, charger, NULL), cnt), 'asc') AS user_parent_root_key,
+	   prod_mdson.MapValueSorter(map_agg(if(user_id != charger, charger_name, NULL), cnt), 'asc') AS user_name_parent_root_key
 FROM root_user
 group by user_id,
          user_real_name,
