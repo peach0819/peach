85a3b7c27b80f33285dd3c7c3d712adab4da6f97
桃 <tao.zheng8833@hipac.cn>
2026-08-04 16:18:48 +0800

    ref

--- 有用的/sql/美赞/2、作战室/ads_crm_visit_user_indicator_detail_v2_d.sql
+++ 有用的/sql/美赞/2、作战室/ads_crm_visit_user_indicator_detail_v2_d.sql
@@ -1,17 +1,16 @@
 INSERT OVERWRITE TABLE ads_crm_visit_user_indicator_detail_v2_d PARTITION (dayid='${v_date}')
 --当月人员拜访达标率需要单独从汇总表里面取
-SELECT data_month,
+SELECT '${v_opt_month}' as data_month,
        user_id,
        'month_visit_reach_rate' as indicator_code,
        null as service_obj_id,
        null as service_obj_name,
        to_json(named_struct(
-           'reach', if_visit_qualified_month_1
+           'reach', get_json_object(get_json_object(biz_value, '$.month_visit_my_reach'), '$.indicator')
        )) as biz_value
-FROM prod_mdson.ads_mdson_user_new_visit_summary_data_d_v2
+FROM prod_mdson.ads_crm_visit_base_summary_d
 WHERE dayid = '${v_date}'
-AND if_visit_qualified_month_1 is not null
-AND data_month = '${v_opt_month}'
+AND get_json_object(biz_value, '$.month_visit_my_reach') is not null
 
 UNION ALL
 
